ec0cde74941a32dfc78d60c022a94c03884f1f6c
Zhang <andrew.haoyu@gmail.com>
2017-09-26 03:21:33 -0400

    casecase icog

--- stractch/scratch_score_support_mixed_model.R
+++ stractch/scratch_score_support_mixed_model.R
@@ -9,6 +9,8 @@ z.design <- matrix(c(
 
 i1 = 1
 library(bc2)
+install_github("andrewhaoyu/bc2", ref = "development",args = c('--library="/home/zhangh24/R/x86_64-pc-linux-gnu-library/3.4"'))
+library(bc2)
 data1 <- read.csv("./data/iCOGS_euro_v10_05242017.csv",header=T)
 y.pheno.mis1 <- cbind(data1$Behaviour1,data1$PR_status1,data1$ER_status1,data1$HER2_status1,data1$Grade1)
 colnames(y.pheno.mis1) = c("Behavior","PR","ER","HER2","Grade")
@@ -24,7 +26,16 @@ x.covar.mis1 <- data1[,5:14]
 x.all.mis1 <- as.matrix(cbind(x.test.all.mis1[,i1],x.covar.mis1))
 colnames(x.all.mis1)[1] <- "gene"
 
-Heter.result.Icog = EMmvpoly(y.pheno.mis1,baselineonly = NULL,additive = x.all.mis1[,2:3],pairwise.interaction = NULL,saturated = NULL,missingTumorIndicator = 888)
+Heter.result.Icog = EMmvpoly(y.pheno.mis1,baselineonly = NULL,additive = x.all.mis1[,2:11],pairwise.interaction = NULL,saturated = NULL,missingTumorIndicator = 888)
+
+M <- 23
+delta0.icog.temp <- Heter.result.Icog[[1]]
+delta0.icog <- c(delta0.icog.temp[1:M],0,delta0.icog.temp[(M+1):length(delta0.icog.temp)])
+
+save(delta0.icog,file = "./whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/delta0.icog.Rdata")
+
+
+
 z.standard <- Heter.result.Icog[[12]]
 z.additive.design <- as.matrix(cbind(1,z.standard))
 M <- nrow(z.standard)
