04aae5d154261cef87f138eacee7eb69e0a38d0a
Zhang <andrew.haoyu@gmail.com>
2017-09-26 12:19:18 -0400

    onco casecase

--- stractch/scratch_score_support_mixed_model.R
+++ stractch/scratch_score_support_mixed_model.R
@@ -31,9 +31,10 @@ Heter.result.Icog = EMmvpoly(y.pheno.mis1,baselineonly = NULL,additive = x.all.m
 M <- 23
 delta0.icog.temp <- Heter.result.Icog[[1]]
 delta0.icog <- c(delta0.icog.temp[1:M],0,delta0.icog.temp[(M+1):length(delta0.icog.temp)])
+z.standard <- Heter.result.Icog[[12]]
 
 save(delta0.icog,file = "./whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/delta0.icog.Rdata")
-
+save(z.standard,file = "./whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/z.standard.Rdata")
 
 
 z.standard <- Heter.result.Icog[[12]]
@@ -75,3 +76,31 @@ score.test.icog<- ScoreTestSelfDesign(y=y.pheno.mis1,
                                       z.design=z.design,
                                       score.test.support=score.test.support.icog,
                                       missingTumorIndicator=888)
+
+
+data2 <- read.csv("./data/Onco_euro_v10_rs554219.csv",header = T)
+#data2 <- read.csv("/data/zhangh20/breast_cancer/known_SNPs_anlysis/Onco_euro_v10_rs554219.csv",1)
+
+# names1 = colnames(data1)[27:206]
+#rm(data1)
+names2 = colnames(data2)
+
+idxi1 = which(names2=="rs554219")
+y.pheno.mis2 <- cbind(data2$Behaviour1,data2$PR_status1,data2$ER_status1,data2$HER2_status1,data2$Grade1)
+colnames(y.pheno.mis2) <- c("Bahavior1","PR","ER","HER2","Grade")
+x.test.all.mis2 <- data2
+
+x.covar.mis2 <- data2[,5:14]
+x.all.mis2 <- as.matrix(cbind(x.test.all.mis2[,idxi1],x.covar.mis2))
+
+
+
+Heter.result.Onco = EMmvpoly(y.pheno.mis2,baselineonly = NULL,additive = x.all.mis2[,2:11],pairwise.interaction = NULL,saturated = NULL,missingTumorIndicator = 888)
+z.standard <- Heter.result.Onco[[12]]
+M <- 23
+delta0.onco.temp <- Heter.result.Onco[[1]]
+delta0.onco <- c(delta0.onco.temp[1:M],0,delta0.onco.temp[(M+1):length(delta0.onco.temp)])
+z.standard <- Heter.result.Onco[[12]]
+
+save(delta0.onco,file = "./whole_genome/ONCO/ERPRHER2GRADE_fixed_baseline/result/delta0.onco.Rdata")
+save(z.standard,file = "./whole_genome/ONCO/ERPRHER2GRADE_fixed_baseline/result/z.standard.Rdata")
