598ee591661bb833bf78b2426fc1caa4325a7dad
Zhang <andrew.haoyu@gmail.com>
2017-10-07 11:45:26 -0400

    update plot

--- whole_genome/ICOG/ERPRHER2_fixed/code/meta_plot_ERPRHER2_fixed.R
+++ whole_genome/ICOG/ERPRHER2_fixed/code/meta_plot_ERPRHER2_fixed.R
@@ -5,10 +5,10 @@ setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRH
 library(qqman)
 if(i1==1){
   load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2_fixed/result/meta_result_shared_1p.Rdata")
-  pvalue <- meta_com_shared_1p[,15]
+  pvalue <- meta_result_shared_1p[,15]
   idx <- which(pvalue<=1.0e-20)
   pvalue[idx] <- 1.0e-20
-  gwas_result <- data.frame(SNP=as.character(meta_com_shared_1p$rs_id),CHR=meta_com_shared_1p$CHR,BP=meta_com_shared_1p$position,
+  gwas_result <- data.frame(SNP=as.character(meta_result_shared_1p$rs_id),CHR=meta_result_shared_1p$CHR,BP=meta_result_shared_1p$position,
                             P=pvalue,stringsAsFactors =F)
   png(paste0("./result/plot/man.png"),width = 7.635,height =4.7175,units = "in",res = 600)
   manhattan(gwas_result,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
