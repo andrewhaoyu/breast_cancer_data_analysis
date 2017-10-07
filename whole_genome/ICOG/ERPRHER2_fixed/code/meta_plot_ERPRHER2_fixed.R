d0063b7168b5719eea9fd3d3448ff742b466e5e8
Zhang <andrew.haoyu@gmail.com>
2017-10-07 11:26:33 -0400

    plot

---
+++ whole_genome/ICOG/ERPRHER2_fixed/code/meta_plot_ERPRHER2_fixed.R
@@ -0,0 +1,40 @@
+args <- commandArgs(trailingOnly=T)
+args <- as.numeric(args[[1]])
+i1 <- args
+setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2_fixed/")
+library(qqman)
+if(i1==1){
+  load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2_fixed/result/meta_result_shared_1p.Rdata")
+  pvalue <- meta_com_shared_1p[,15]
+  idx <- which(pvalue<=1.0e-20)
+  pvalue[idx] <- 1.0e-20
+  gwas_result <- data.frame(SNP=as.character(meta_com_shared_1p$rs_id),CHR=meta_com_shared_1p$CHR,BP=meta_com_shared_1p$position,
+                            P=pvalue,stringsAsFactors =F)
+  png(paste0("./result/plot/man.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwasresult,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
+  dev.off()
+  
+}else{
+  load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2_fixed/result/meta_result_shared_1p_filter.Rdata")
+  pvalue <- meta_result_shared_1p_filter[,15]
+  idx <- which(pvalue<=1.0e-20)
+  pvalue[idx] <- 1.0e-20
+  gwas_result <- data.frame(SNP=as.character(meta_result_shared_1p_filter$rs_id),CHR=meta_result_shared_1p_filter$CHR,BP=meta_result_shared_1p_filter$position,
+                            P=pvalue,stringsAsFactors =F)
+  png(paste0("./result/plot/man.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwasresult,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
+  dev.off()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
