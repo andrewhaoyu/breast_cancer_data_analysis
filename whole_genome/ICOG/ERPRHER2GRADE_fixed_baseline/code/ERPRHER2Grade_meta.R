86b1779760a7506f8230e797357aff5bd6950ae7
Zhang <andrew.haoyu@gmail.com>
2017-10-11 01:13:29 -0400

    update

---
+++ whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/code/ERPRHER2Grade_meta.R
@@ -0,0 +1,110 @@
+load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/icog_result_shared_1p.Rdata")
+load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ONCO/ERPRHER2GRADE_fixed_baseline/result/onco_result_shared_1p.Rdata")
+library(bc2)
+second.num <- 5
+
+
+icog_score_infor <- icog_result_shared_1p[,11:(11+second.num+second.num^2-1)]
+onco_score_infor <- onco_result_shared_1p[,11:(11+second.num+second.num^2-1)]
+
+
+
+
+icog_onco_score_infor <- cbind(icog_score_infor,onco_score_infor)
+meta_result_shared_1p <- icog_result_shared_1p[,c(1:10,(ncol(icog_result_shared_1p)-3):ncol(icog_result_shared_1p))]
+
+rm(icog_result_shared_1p)
+rm(onco_result_shared_1p)
+gc()
+
+library(foreach)
+library(doParallel)
+no.cores <- 5
+n <- nrow(icog_onco_score_infor)
+#icog_onco_score_infor_temp <- icog_onco_score_infor[1:10^5,]
+#n <- nrow(icog_onco_score_infor_temp)
+#pvalue <- rep(0,n)
+size <- 1000
+#n <- nrow(icog_onco_score_infor)
+
+
+
+registerDoParallel(no.cores)
+#pvalue <- rep(0,nrow(icog_onco_score_infor))
+
+
+pvalue.list <- foreach(i=1:size)%dopar%
+{
+  print(i)
+  start.end <- startend(n,size,i)
+  start <- start.end[1]
+  end <- start.end[2]
+  pvalue_sub <- rep(0,end-start+1)
+  temp = 1
+  for(j in start:end){
+    icog_onco_score_infor_oneline <- icog_onco_score_infor[j,]
+    pvalue_sub[temp] <- MetaPfunction(icog_onco_score_infor_oneline,second.num)
+    temp = temp+1
+  }
+  return(pvalue_sub)
+  
+  
+}
+stopImplicitCluster()
+
+pvalue <- rep(0,n)
+temp.total = 0
+for(i in 1:size){
+  print(i)
+  temp <- length(pvalue.list[[i]])
+  pvalue[(1:temp)+temp.total] <- pvalue.list[[i]]
+  temp.total <- temp+temp.total
+  
+  
+}
+
+
+
+meta_result_shared_1p <- cbind(meta_result_shared_1p,pvalue)
+
+
+
+
+
+save(meta_result_shared_1p,file=paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p.Rdata"))
+
+known_snps <- read.csv("/spin1/users/zhangh24/breast_cancer_data_analysis/data/known_SNPs_info.csv",header= T)
+
+
+
+
+
+
+
+idx_cut <- NULL
+
+
+for(i in 1:nrow(known_snps)){
+  print(i)
+  chr_temp <- known_snps[i,3]
+  position_temp <- known_snps[i,4]
+  position_low <- position_temp-0.5*10^6
+  position_high <- position_temp+0.5*10^6
+  idx <- which(meta_result_shared_1p$CHR==chr_temp&meta_result_shared_1p$position>position_low&
+                 meta_result_shared_1p$position<position_high)
+  idx_cut <- c(idx_cut,idx)
+}
+############duplicate variables won't mater
+idx_cut <- unique(idx_cut)
+meta_result_shared_1p_filter <- meta_result_shared_1p[-idx_cut,]
+
+save(meta_result_shared_1p_filter,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p_filter.Rdata")
+
+
+
+
+
+
+
+
+
---
+++ whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/code/plot_fixed_ERPRHER2GRADE.R
@@ -0,0 +1,70 @@
+args <- commandArgs(trailingOnly=T)
+args <- as.numeric(args[[1]])
+i1 <- args
+setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline")
+library(qqman)
+if(i1==1){
+  load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p.Rdata")
+  pvalue <- meta_result_shared_1p[,15]
+  idx <- which(pvalue<=1.0e-20)
+  pvalue[idx] <- 1.0e-20
+  gwas_result <- data.frame(SNP=as.character(meta_result_shared_1p$rs_id),CHR=meta_result_shared_1p$CHR,BP=meta_result_shared_1p$position,
+                            P=pvalue,stringsAsFactors =F)
+  png(paste0("./result/plot/man.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwas_result,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
+  dev.off()
+  
+}else if(i1==2){
+  load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p_filter.Rdata")
+  pvalue <- meta_result_shared_1p_filter[,15]
+  idx <- which(pvalue<=1.0e-20)
+  pvalue[idx] <- 1.0e-20
+  gwas_result <- data.frame(SNP=as.character(meta_result_shared_1p_filter$rs_id),CHR=meta_result_shared_1p_filter$CHR,BP=meta_result_shared_1p_filter$position,
+                            P=pvalue,stringsAsFactors =F)
+  png(paste0("./result/plot/man_filter.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwas_result,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
+  dev.off()
+}else{
+  load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p_filter.Rdata")
+  new_filter <- read.csv("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2_fixed/result/Filter_based_on_Montse.csv",header=T,stringsAsFactors = F)
+  new_filter[,2] <- as.numeric(gsub(",","",new_filter[,2]))
+  
+  pvalue <- meta_result_shared_1p_filter[,15]
+  idx <- which(pvalue<=1.0e-20)
+  pvalue[idx] <- 1.0e-20
+  gwas_result <- data.frame(SNP=as.character(meta_result_shared_1p_filter$rs_id),CHR=meta_result_shared_1p_filter$CHR,BP=meta_result_shared_1p_filter$position,
+                            P=pvalue,stringsAsFactors =F)
+  idx_cut <- NULL
+  
+  
+  for(i in 1:nrow(new_filter)){
+    print(i)
+    chr_temp <- new_filter[i,3]
+    position_temp <- new_filter[i,2]
+    position_low <- position_temp-0.5*10^6
+    position_high <- position_temp+0.5*10^6
+    idx <- which(meta_result_shared_1p_filter$CHR==chr_temp&meta_result_shared_1p_filter$position>position_low&
+                   meta_result_shared_1p_filter$position<position_high)
+    idx_cut <- c(idx_cut,idx)
+  }
+  idx_cut <- unique(idx_cut)
+  gwas_result <- gwas_result[-idx_cut,]
+  
+  png(paste0("./result/plot/man_filter_Montse.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwas_result,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
+  dev.off()
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
