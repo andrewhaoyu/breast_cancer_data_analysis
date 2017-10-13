374c9f5ef6269ffaa5f97ed88d2ee96ad865ed7b
Zhang <andrew.haoyu@gmail.com>
2017-10-13 16:41:00 -0400

    update

--- whole_genome/ICOG/ERPRHER2_fixed/code/plot_ERPRHER2_fixed.R
+++ whole_genome/ICOG/ERPRHER2_fixed/code/plot_ERPRHER2_fixed.R
@@ -42,6 +42,20 @@ if(i1==1){
   
 }
 
+position.cut = 0.5*10^6
+
+
+idx.cut.more <- which(gwas_result$P<=5E-08&gwas_result$CHR==5&(gwas_result[,3]>=(46146805-position.cut)&gwas_result[,3]<=(46146805+position.cut)))
+idx <- which(gwas_result$P<=5E-08&gwas_result$CHR==1)
+idx.cut.more <- c(idx,idx.cut.more)
+gwas_result <- gwas_result[-idx.cut.more,]
+idx <- which(gwas_result$CHR==15&gwas_result$BP==67408298)
+gwas_result[idx,4] <- 5E-08
+
+
+png(paste0("./result/plot/man_filter_Montse_dirty.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+manhattan(gwas_result,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
+dev.off()
 
 
 
