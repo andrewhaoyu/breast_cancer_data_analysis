4bf5f1022d04c2c1a485917210cb7e162f78fc13
Zhang <andrew.haoyu@gmail.com>
2017-11-07 16:43:59 -0500

    find shared snps

--- whole_genome_age/ONCO/ERPRHER2GRADE_casecase/code/ERPRHER2Grade_casecase_merge_onco.R
+++ whole_genome_age/ONCO/ERPRHER2GRADE_casecase/code/ERPRHER2Grade_casecase_merge_onco.R
@@ -3,6 +3,8 @@ Files <- dir(Filesdir,pattern="OncoArray_european_merged_b1_15.",full.names=T)
 Filesex <- dir(Filesdir,pattern="OncoArray_european_merged_b1_15.chr23",full.names=T)
 idx.sex <- Files%in%Filesex
 Files <- Files[!idx.sex]
+library(gtools)
+Files <- mixedsort(Files)
 
 Files <- gsub("/gpfs/gsfs4/users/NC_BW/icogs_onco/genotype/imputed2/onco_imputed/OncoArray_european_merged_b1_15.","",Files)
 Files <- gsub(".txt.gz","",Files)
@@ -23,7 +25,7 @@ for(i in 1:length(Files)){
 }
 idx <- order(Files_sub$chr,Files_sub$p1)
 File_sub_order <- Files_sub[order(Files_sub$chr,Files_sub$p1),]
-result.dir <- "/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ONCO/ERPRHER2GRADE_fixed_baseline/result/"
+result.dir <- "/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/"
 result_Files <- dir(result.dir,pattern="ERPRHER2Grade_fixed_onco")
 result_Files <- result_Files[1:567]
 result.idx <- rep(0,length(result_Files))
@@ -35,17 +37,17 @@ for(i in 1:length(result_Files)){
 
 
 #load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ONCO/ERPRHER2GRADE_fixed_baseline/result/onco_result.Rdata")
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ONCO/ERPRHER2GRADE_fixed_baseline/result/onco_result_baseline.Rdata")
-num.total <- nrow(onco_result_baseline)
+load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/onco_result.Rdata")
+num.total <- nrow(onco_result)
 
 
 
 num <- num.total
 #num.total <- 0
-rs_id <- onco_result_baseline$rs_id
+rs_id <- rep("c",num.total)
 number.of.tumor <- 4
-score <- matrix(0,nrow=num,ncol = (number.of.tumor))
-infor <- matrix(0,nrow = num,ncol = (number.of.tumor)^2)
+score <- matrix(0,nrow=num,ncol = (number.of.tumor-1))
+infor <- matrix(0,nrow = num,ncol = (number.of.tumor-1)^2)
 freq.all <- rep(0,num)
 #score_baseline <- rep(0,num)
 #infor_baseline <- rep(0,num)
@@ -54,34 +56,43 @@ freq.all <- rep(0,num)
 
 num.total <- 0
 for(i in 1:567){
-  
-  print(i)
-  for (k in 1:5) {
+  if(i!=219){
+    print(i)
+    for (k in 1:5) {
+      load(
+        paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_casecase/result/ERPRHER2Grade_casecase_onco",idx[i],"_",k)
+      )
+      temp <- nrow(result[[2]])
+      rs_id[num.total+(1:temp)] <- result[[1]]
+      score[num.total+(1:temp),] <- result[[2]]
+      infor[num.total+(1:temp),] <- result[[3]]
+      freq.all[num.total+(1:temp)] <- result[[4]] 
+      num.total <- temp+num.total
+    }  
+  }else{
     load(
-      paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ONCO/ERPRHER2GRADE_casecase/result/ERPRHER2Grade_casecase_onco",idx[i],"_",k)
+      paste0("./whole_genome_age/ONCO/ERPRHER2GRADE_casecase/result/ERPRHER2Grade_casecase_onco",idx[i])
     )
     temp <- nrow(result[[2]])
-    #print(paste0("temp:",temp))
-    #rs_id[num.total+(1:temp)] <- result[[1]]
+    rs_id[num.total+(1:temp)] <- result[[1]]
     score[num.total+(1:temp),] <- result[[2]]
-    for(j in 1:temp){
-      infor_j <- result[[3]][((number.of.tumor)*j-(number.of.tumor-1)):((number.of.tumor)*j),]
-      infor[num.total+j,] <- as.vector(infor_j)
-    }
-   # freq.all[num.total+(1:temp)] <- result[[4]] 
+    infor[num.total+(1:temp),] <- result[[3]]
+    freq.all[num.total+(1:temp)] <- result[[4]] 
     num.total <- temp+num.total
   }
+  
   
   
   
 }
 
 
+load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ONCO/ERPRHER2GRADE_fixed_baseline/result/onco_info.Rdata")
+CHR <- onco_info[,11]
+onco_info <- onco_info[,1:10]
 
-onco_info <- onco_result_baseline[,1:10]
-CHR <- onco_result_baseline[,13]
 
 onco_result_casecase <- data.frame(onco_info,score,infor,CHR)
 
-save(onco_result_casecase,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ONCO/ERPRHER2GRADE_casecase/result/onco_result_casecase.Rdata")
+save(onco_result_casecase,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_casecase/result/onco_result_casecase.Rdata")
 print(1)
---
+++ whole_genome_age/ONCO/ERPRHER2GRADE_casecase/code/casecsae_resubmit_merge_232.R
@@ -0,0 +1,31 @@
+setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/")
+num.total <- 0
+for(i1 in 1){
+  for(i2 in 1:1000){
+    print(i2)
+    load(paste0("./whole_genome_age/ONCO/ERPRHER2GRADE_casecase/result/ERPRHER2Grade_casecase_onco_resubmit",i1,"_",i2))
+    temp <- length(result[[1]])
+    num.total <- temp+num.total
+  }
+}
+
+snpid <- rep("c",num.total)
+score <- matrix(0,num.total,3)
+infor <- matrix(0,num.total,9)
+freq <- rep(0,num.total)
+num.total <- 0
+for(i1 in 1){
+  for(i2 in 1:1000){
+    print(i2)
+    load(paste0("./whole_genome_age/ONCO/ERPRHER2GRADE_casecase/result/ERPRHER2Grade_casecase_onco_resubmit",i1,"_",i2))
+    temp <- length(result[[1]])
+    snpid[num.total+(1:temp)] <- result[[1]]
+    score[num.total+(1:temp),] <- result[[2]]
+    infor[num.total+(1:temp),] <- result[[3]]
+    freq[num.total+(1:temp)] <- result[[4]]
+    num.total <- temp+num.total
+  }
+}
+
+result <- list(snpid,score,infor,freq)
+save(result,file=paste0("./whole_genome_age/ONCO/ERPRHER2GRADE_casecase/result/ERPRHER2Grade_casecase_onco",i1))
