9ecee007b1f5303fb722a954e6ad26c86ddf8404
Zhang <andrew.haoyu@gmail.com>
2017-11-10 08:58:34 -0500

    update

--- whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/code/extract_SNP_icog.R
+++ whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/code/extract_SNP_icog.R
@@ -1,49 +1,49 @@
-rm(list=ls())
-#args=(commandArgs(TRUE))
-#for(p in 1:length(args)){
-#       eval(parse(text=args[[p]]))
-#  }
-#print(i)
-#i1 <- i
-#commandarg <- commandArgs(trailingOnly=F)
-#myarg <- commandarg[length(commandarg)]
-#myarg <- sub("-","",myarg)
-#i <- as.numeric(myarg)
-#print(i)
-#pheno is ICOGS,data2 is onco_array
-arg <- commandArgs(trailingOnly=T)
-i <- as.numeric(arg[[1]])
-i1 <- i
-print(i)
+
 library(R.utils)
+library(data.table)
 setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/")
 
 n <- 109713
 snpvalue <- rep(0,n)
 subject.file <- "/gpfs/gsfs4/users/NC_BW/icogs_onco/genotype/imputed2/icogs_order.txt.gz"
 Icog.order <- read.table(gzfile(subject.file))
-pheno.file <- "./data/pheno.Icog"
-load(pheno.file)
-n.sub = nrow(pheno)
-y.pheno.mis1 <- cbind(pheno$Behaviour1,pheno$PR_status1,pheno$ER_status1,pheno$HER2_status1)
-colnames(y.pheno.mis1) = c("Behaviour1","PR_status1",
-                           "ER_status1","HER2_status1")
-idx.fil <- Icog.order[,1]%in%pheno$SG_ID
-idx.match <- match(pheno$SG_ID,Icog.order[idx.fil,1])
+
+setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/")
+data1 <- fread("./data/iCOGS_euro_v10_10232017.csv",header=T)
+data1 <- as.data.frame(data1)
+y.pheno.mis1 <- cbind(data1$Behaviour1,data1$ER_status1,data1$PR_status1,data1$HER2_status1,data1$Grade1)
+colnames(y.pheno.mis1) = c("Behavior","ER","PR","HER2","Grade")
+#x.test.all.mis1 <- data1[,c(27:206)]
+SG_ID <- data1$SG_ID
+x.covar.mis1 <- data1[,c(5:14,204)]
+age <- data1[,204]
+idx.complete <- which(age!=888)
+y.pheno.mis1 <- y.pheno.mis1[idx.complete,]
+x.covar.mis1 <- x.covar.mis1[idx.complete,]
+SG_ID <- SG_ID[idx.complete]
+
+
+
+
+idx.fil <- Icog.order[,1]%in%SG_ID
+idx.match <- match(SG_ID,Icog.order[idx.fil,1])
 #Icog.order.match <- Icog.order[idx.fil,1][idx.match]
 library(bc2)
-#load("./whole_genome/ICOG/ERPRHER2_fixed/result/score.test.support.icog.ERPRHER2.Rdata")
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_list.Rdata")
+#load("./whole_genome_age/ICOG/ERPRHER2_fixed/result/score.test.support.icog.ERPRHER2.Rdata")
+load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_list.Rdata")
+
 
 snp.icogs.extract.id <- extract.list[,(ncol(extract.list)-2),drop=F]
-write.table(snp.icogs.extract.id,file = paste0("./whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_id_icog.txt"),quote = F,row.names=F)
-Filesdir <- "/gpfs/gsfs4/users/NC_BW/icogs_onco/genotype/imputed2/icogs_imputed/"
+write.table(snp.icogs.extract.id,file = paste0("./whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_id_icog.txt"),quote = F,row.names=F)
 
+Filesdir <- "/gpfs/gsfs4/users/NC_BW/icogs_onco/genotype/imputed2/icogs_imputed/"
 Files <- dir(Filesdir,pattern="icogs_merged_b1_12.",full.names=T)
 Filesex <- dir(Filesdir,pattern="icogs_merged_b1_12.chr23",full.names=T)
 idx.sex <- Files%in%Filesex
 Files <- Files[!idx.sex]
-geno.file <- Files[i1]
+library(gtools)
+Files <- mixedsort(Files)
+
 
 
 qctool.command <- rep("c",564)
@@ -52,16 +52,12 @@ qctool.command <- data.frame(qctool.command,stringsAsFactors=F)
 
 for(i in 1:564){
   geno.file <- Files[i]
-  temp <- paste0("/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g ",Files[i]," -incl-rsids /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_id_icog.txt -og /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/ERPRHER2_extract",i,".txt")
+  temp <- paste0("/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g ",Files[i]," -incl-rsids /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_id_icog.txt -og /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/ERPRHER2_extract",i,".txt")
   qctool.command[i,1] <- temp
   
 }
 
 
-write.table(qctool.command,file = paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/code/qc_extract_snp.sh"),col.names = F,row.names = F,quote=F)
-#qctool.command <- as.data.frame(qctool.command,stringsAsFactors=F)
-
-
-
+write.table(qctool.command,file = paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/code/qc_extract_snp.sh"),col.names = F,row.names = F,quote=F)
 
 
--- whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/code/generate_extract_list.R
+++ whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/code/generate_extract_list.R
@@ -1,6 +1,6 @@
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p_filter_1M.Rdata")
-
-idx <- which(meta_result_shared_1p_filter$pvalue<(5*10^-6))
+load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p_filter_1M_Ju.Rdata")
+meta_result_shared_1p_filter <- meta_result_shared_1p_filter_Ju
+idx <- which(meta_result_shared_1p_filter$p.value<(5*10^-6))
 
 
 
@@ -19,4 +19,15 @@ extract.list <- meta_result_shared_1p_filter[idx,]
 
 
 
-save(extract.list,file = paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_list.Rdata"))
+save(extract.list,file = paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_list.Rdata"))
+
+
+
+
+
+
+
+
+
+
+
