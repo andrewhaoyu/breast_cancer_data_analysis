c3e43f11828c663690012305b2b7d22106fc6209
Zhang <andrew.haoyu@gmail.com>
2017-11-10 10:51:15 -0500

    update

--- whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/code/generate_Julie_SNPs_list.R
+++ whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/code/generate_Julie_SNPs_list.R
@@ -38,11 +38,42 @@ qctool.command <- data.frame(qctool.command,stringsAsFactors=F)
 
 for(i in 1:564){
   geno.file <- Files[i]
-  temp <- paste0("/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g ",Files[i]," -incl-rsids /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_id_icog.txt -og /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/ERPRHER2_extract",i,".txt")
+  temp <- paste0("/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g ",Files[i]," -incl-rsids /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_id_icog_Julie.txt -og /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/Julie_Icog",i,".txt")
   qctool.command[i,1] <- temp
   
 }
 
 
 write.table(qctool.command,file = paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/code/qc_extract_snp_julie.sh"),col.names = F,row.names = F,quote=F)
+
+
+
+
+
+Filesdir <- "/gpfs/gsfs4/users/NC_BW/icogs_onco/genotype/imputed2/onco_imputed"
+Files <- dir(Filesdir,pattern="OncoArray_european_merged_b1_15.",full.names=T)
+Filesex <- dir(Filesdir,pattern="OncoArray_european_merged_b1_15.chr23",full.names=T)
+idx.sex <- Files%in%Filesex
+Files <- Files[!idx.sex]
+Files <- mixedsort(Files)
+
+
+qctool.command <- rep("c",567)
+qctool.command <- data.frame(qctool.command,stringsAsFactors=F)
+
+
+for(i in 1:567){
+  geno.file <- Files[i]
+  temp <- paste0("/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g ",Files[i]," -incl-rsids /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/extract_id_onco_Julie.txt -og /spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/Julie_onco",i,".txt")
+  qctool.command[i,1] <- temp
+  
+}
+
+
+write.table(qctool.command,file = paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/code/qc_extract_snp_julie.sh"),col.names = F,row.names = F,quote=F)
+
+
+
+
+
 
