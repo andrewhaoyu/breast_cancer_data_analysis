a0bdcebc342bd005b1ec4be84a51cd36cc54919a
Zhang <andrew.haoyu@gmail.com>
2017-12-01 14:19:06 -0500

    type one error simulation merge

--- simulation/type_one_error/code/merge_simulation_type_one_error.R
+++ simulation/type_one_error/code/merge_simulation_type_one_error.R
@@ -7,11 +7,26 @@ for(i in 1:1000){
   load(paste0("pvalue",i,".Rdata"))
   temp <- nrow(p.value.simulation)
   simulation.result[(1:temp)+total,] <- p.value.simulation
+  total <- temp+total
 }
 dim(simulation.result)
 hist(simulation.result[,1])
 library(qqman)
-qq(simulation.result[,5])
+p.value <- simulation.result[,1]
+qqman::qq(simulation.result[,6])
+
+library(lattice)
+qqmath(~-log10(p.value),
+       distribution=function(x){-log10(qunif(1-x))}
+       )
 save(simulation.result,file="/spin1/users/zhangh24/breast_cancer_data_analysis/simulation/type_one_error/result/type_one_error_combine.Rdata")
 
 load("breast_cancer_data_analysis/simulation/type_one_error/result/type_one_error_combine.Rdata")
+
+
+my.pvalues<-runif(10000)
+
+library(lattice);
+qqmath(~-log10(my.pvalues),
+       distribution=function(x){-log10(qunif(1-x))}
+);
