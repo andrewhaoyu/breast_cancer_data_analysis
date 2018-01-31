553c714dc874c260a90f331372af515d836a9979
Zhang <andrew.haoyu@gmail.com>
2018-01-31 02:31:16 -0500

    icog transform

---
+++ genetic_correlation/ICOG/code/icog_clean.r
@@ -0,0 +1,46 @@
+setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/")
+
+total <- 0
+sig <- c(1:564)
+dele  = c(56,160,281,291,292,299,311,350,351,416,421,422,435)
+sig <- sig[-dele]
+
+
+
+for(i1 in sig){
+  print(i1)
+  load(paste0("./genetic_correlation/ICOG/result/intrinsic_i1",i1))  
+  total <- total+length(result[[1]])
+}
+
+sigma <- matrix(n,total,25)
+logodds <- matrix(n,total,5)
+snpid <- rep("c",total)
+total <- 0
+for(i1 in sig){
+  print(i1)
+  load(paste0("./genetic_correlation/ICOG/result/intrinsic_i1",i1))   
+  temp <- length(result[[1]])
+  snpid[total+(1:temp)] <- result[[1]]
+  logodds[total+(1:temp),] <- result[[2]]
+  sigma[total+(1:temp),] <- result[[3]]
+  total <- total+ temp
+}
+
+
+
+
+ICOG.result <- data.frame(SNP.ICOGS=snpid,logodds,sigma)
+
+
+ICOG.result.clean <- merge(shared.data,ICOG.result,by.x="SNP.ICOGS",
+                           by.y = "SNP.ICOGS")
+
+save(ICOG.result.clean,file= "./genetic_correlation/result/ICOG.result.clean.Rdata")
+
+#load("./genetic_correlation/result/hapmap3list.Rdata")
+
+
+
+
+#dele  = c(56,160,281,291,292,299,311,350,351,416,421,422,435)
---
+++ genetic_correlation/ICOG/code/icog_transform.R
@@ -0,0 +1,27 @@
+arg <- commandArgs(trailingOnly=T)
+i1 <- as.numeric(arg[[1]])
+z.design <- matrix(c(
+  c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
+  c(0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1),
+  c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0),
+  c(0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0),
+  c(1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0)
+),ncol=5)
+
+load("./genetic_correlation/result/ICOG.result.clean.Rdata")
+n <- nrow(ICOG.result.clean)
+library(bc2)
+size = 1000
+start.end<- startend(n,size,i1)
+start <- start.end[1]
+end <- start.end[2]
+
+result.sub <- matrix(0,(end-start+1),30)
+for(i in start:end){
+  logodds <- as.numeric(as.vector(ICOG.result.clean[i,11:15]))
+  sigma <- matrix(as.numeric(ICOG.result.clean[i,16:40]),5,5)  
+  result.sub[i,] <- transfunction(logodds,sigma)
+}
+
+
+save(result.sub,paste0("./genetic_correlation/ICOG/result.sub",i1,".Rdata"))
