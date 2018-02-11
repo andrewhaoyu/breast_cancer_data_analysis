63a796b9eb64a1a38467ca249d89682a13c3b2d5
Zhang <andrew.haoyu@gmail.com>
2018-02-11 00:15:58 -0500

    intrinsic_subtype_icog

--- whole_genome_age/ICOG/Intrinsic_subtypes/code/intrinsic_subtype_icog.R
+++ whole_genome_age/ICOG/Intrinsic_subtypes/code/intrinsic_subtype_icog.R
@@ -220,5 +220,5 @@ for(i in 1:inner.size){
 
 result <- list(snpid_reuslt=snpid_result,score_result=score_result,infor_result=infor_result,freq.all=freq.all)
 
-save(result,file=paste0("./whole_genome_age/ICOG/ERPRHER2GRADE_casecase/result/ERPRHER2Grade_casecase",i1,"_",i2))
+save(result,file=paste0("./whole_genome_age/ICOG/Intrinsic_subtypes/intrinsic_subytpe_icog",i1,"_",i2))
 
