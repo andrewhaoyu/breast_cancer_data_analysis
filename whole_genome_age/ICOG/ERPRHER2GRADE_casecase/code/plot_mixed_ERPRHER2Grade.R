0ee570fa48a021e858a2b5054f4e8b5b6895ab04
Zhang <andrew.haoyu@gmail.com>
2018-04-23 16:08:04 -0400

    update plot

--- whole_genome_age/ICOG/ERPRHER2GRADE_casecase/code/plot_mixed_ERPRHER2Grade.R
+++ whole_genome_age/ICOG/ERPRHER2GRADE_casecase/code/plot_mixed_ERPRHER2Grade.R
@@ -10,32 +10,24 @@ if(i1==1){
   pvalue[idx] <- 1.0e-20
   gwas_result <- data.frame(SNP=as.character(meta_result_shared_1p$rs_id),CHR=meta_result_shared_1p$CHR,BP=meta_result_shared_1p$position,
                             P=pvalue,stringsAsFactors =F)
-  observed <- sort(gwas_result$P)
-  lobs <- -(log10(observed))
-  
-  expected <- c(1:length(observed)) 
-  lexp <- -(log10(expected / (length(expected)+1)))
-  png(paste0("./result/plot/qq.png"),width = 7.635,height =4.7175,units = "in",res = 600)
-  plot(c(0,8), c(0,20), col="red", lwd=3, type="l", xlab="Expected (-logP)", ylab="Observed (-logP)", xlim=c(0,8), ylim=c(0,20), las=1, xaxs="i", yaxs="i", bty="l",main=paste0(" Global Test Association Manhattan QQ Plot"))
-  points(lexp, lobs, pch=23, cex=.4, bg="black") 
-  dev.off()
+
   png(paste0("./result/plot/man.png"),width = 7.635,height =4.7175,units = "in",res = 600)
   manhattan(gwas_result,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
   dev.off()
-
- # qq(gwas_result$P,main=paste0(" Global Test Association Manhattan QQ Plot"))
-  #dev.off()
+  png(paste0("./result/plot/qq.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  qq(gwas_result$P,main=paste0(" Global Test Association Manhattan QQ Plot"),xlim=c(0,8),ylim=c(0,20))
+  dev.off()
   
 }else if(i1==2){
   load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_casecase/result/meta_result_shared_1p_filter_1M.Rdata")
-  load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract.list.ld")
+ # load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract.list.ld")
   pvalue <- meta_result_shared_1p_filter[,15]
   idx <- which(pvalue<=1.0e-20)
   pvalue[idx] <- 1.0e-20
   gwas_result <- data.frame(SNP=as.character(meta_result_shared_1p_filter$rs_id),CHR=meta_result_shared_1p_filter$CHR,BP=meta_result_shared_1p_filter$position,
                             P=pvalue,stringsAsFactors =F)
-  idx.cut <- which((gwas_result$BP%in%extract.list.ld$position)&(gwas_result$CHR%in%extract.list.ld$CHR))
-  gwas_result <- gwas_result[-idx.cut,]
+  # idx.cut <- which((gwas_result$BP%in%extract.list.ld$position)&(gwas_result$CHR%in%extract.list.ld$CHR))
+  # gwas_result <- gwas_result[-idx.cut,]
   png(paste0("./result/plot/man_filter.png"),width = 7.635,height =4.7175,units = "in",res = 600)
   manhattan(gwas_result,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
   dev.off()
@@ -44,7 +36,7 @@ if(i1==1){
   dev.off()
 }else{
   load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_casecase/result/meta_result_shared_1p_filter_1M_Ju.Rdata")
-  load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract.list.ld")
+  #load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract.list.ld")
   meta_result_shared_1p_filter <- meta_result_shared_1p_filter_Ju
   
   pvalue <- meta_result_shared_1p_filter[,15]
@@ -52,20 +44,11 @@ if(i1==1){
   pvalue[idx] <- 1.0e-20
   gwas_result <- data.frame(SNP=as.character(meta_result_shared_1p_filter$rs_id),CHR=meta_result_shared_1p_filter$CHR,BP=meta_result_shared_1p_filter$position,
                             P=pvalue,stringsAsFactors =F)
-  idx.cut <- which((gwas_result$BP%in%extract.list.ld$position)&(gwas_result$CHR%in%extract.list.ld$CHR))
-  gwas_result <- gwas_result[-idx.cut,]
-  
-  observed <- sort(gwas_result$P)
-  lobs <- -(log10(observed))
-  
-  expected <- c(1:length(observed)) 
-  lexp <- -(log10(expected / (length(expected)+1)))
-  
-  
-  
-  png(paste0("./result/plot/qq_filter_Julie.png"),width = 7.635,height =4.7175,units = "in",res = 600)
-  plot(c(0,8), c(0,15), col="red", lwd=3, type="l", xlab="Expected (-logP)", ylab="Observed (-logP)", xlim=c(0,8), ylim=c(0,15), las=1, xaxs="i", yaxs="i", bty="l",main=paste0(" Global Test Association Manhattan QQ Plot"))
-  points(lexp, lobs, pch=23, cex=.4, bg="black") 
+  png(paste0("./result/plot/man_filter_Ju.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwas_result,suggestiveline = F, cex.axis = 2,main=paste0(" Global Test Association Manhattan Plot"),ylim=c(0,20))
+  dev.off()
+  png(paste0("./result/plot/qq_filter_Ju.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  qq(gwas_result$P,main=paste0(" Global Test Association Manhattan QQ Plot"),ylim=c(0,15),xlim=c(1,8))
   dev.off()
   
 }
