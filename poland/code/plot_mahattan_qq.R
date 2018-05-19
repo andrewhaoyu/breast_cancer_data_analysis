abfe89e53078ddd248d0726b66a5b428bb1e2127
Zhang <andrew.haoyu@gmail.com>
2018-05-19 16:15:06 -0400

    plot mahattan

--- poland/code/plot_mahattan_qq.R
+++ poland/code/plot_mahattan_qq.R
@@ -4,7 +4,7 @@ load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_geno
 load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_casecase_5p.Rdata")
 onco_result_fixed_5p
 gwas_result1 <- data.frame(SNP=as.character(onco_result_fixed_5p$rs_id),CHR=onco_result_fixed_5p$CHR,BP=onco_result_fixed_5p$position,
-                          P=onco_result_fixed_5p,stringsAsFactors =F)
+                          P=onco_result_fixed_5p$P,stringsAsFactors =F)
 
 png(paste0("./man_fix.png"),width = 7.635,height =4.7175,units = "in",res = 600)
 manhattan(gwas_result1,suggestiveline = F, cex.axis = 2,main=paste0("Global Test Association Manhattan Plot"))
@@ -13,7 +13,7 @@ png(paste0("./qq_random.png"),width = 7.635,height =4.7175,units = "in",res = 60
 qq(gwas_result1$P,main=paste0("Global Test Association Manhattan QQ Plot"))
 dev.off()
 gwas_result2 <- data.frame(SNP=as.character(onco_result_casecase_5p$rs_id),CHR=onco_result_casecase_5p$CHR,BP=onco_result_casecase_5p$position,
-                           P=onco_result_casecase_5p,stringsAsFactors =F)
+                           P=onco_result_casecase_5p$P,stringsAsFactors =F)
 
 png(paste0("./man_random.png"),width = 7.635,height =4.7175,units = "in",res = 600)
 manhattan(gwas_result2,suggestiveline = F, cex.axis = 2,main=paste0("Global Test Association Manhattan Plot"))
