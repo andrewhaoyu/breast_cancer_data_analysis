d0ac94cbe05f667ce146ef4e8875310604050b30
Zhang <andrew.haoyu@gmail.com>
2018-05-22 13:12:18 -0400

    standard analysis

--- poland/code/standard_analysis.R
+++ poland/code/standard_analysis.R
@@ -145,7 +145,11 @@ idx.control <- which(y.pheno.mis2[,1]==0)
 n.control <- length(idx.control)
 
 
-
+size = 5
+start.end <- startend(num,size,i2)
+start <- start.end[1]
+end <- start.end[2]
+file.num <- end-start+1
 
 
 no.cores <- 2
