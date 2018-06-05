3f4f89a6f3fc44e22a278b5066ca366a5b8d1199
Zhang <andrew.haoyu@gmail.com>
2018-06-05 13:03:39 -0400

    plot_mahattan

--- poland/code/plot_mahattan_qq.R
+++ poland/code/plot_mahattan_qq.R
@@ -3,7 +3,10 @@ library(qqman)
 #load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_fixed_5p.Rdata")
 #load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_casecase_5p.Rdata")
 load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_standard_5p.Rdata")
-
+# idx <- which(onco_result_standard_5p$p_value1<=5E-08)
+# onco_result_standard_5p[idx,]
+# idx2 <- which(onco_result_casecase_5p$P<=5E-08)
+# onco_result_casecase_5p[idx2,]
 #onco_result_fixed_5p
 # gwas_result1 <- data.frame(SNP=as.character(onco_result_fixed_5p$rs_id),CHR=onco_result_fixed_5p$CHR,BP=onco_result_fixed_5p$position,
 #                           P=onco_result_fixed_5p$P,stringsAsFactors =F)
@@ -45,7 +48,7 @@ idx <- which(p_value2 <= 1E-10)
 p_value2[idx] <- 1
 gwas_result4 <- data.frame(SNP=as.character(onco_result_standard_5p$rs_id),CHR=onco_result_standard_5p$CHR,BP=onco_result_standard_5p$position,
                            P=p_value2,stringsAsFactors =F)
-idx <- which(p_value==1)
+idx <- which(p_value2==1)
 gwas_result4 <- gwas_result4[-idx,]
 png(paste0("./man_poly.png"),width = 7.635,height =4.7175,units = "in",res = 600)
 manhattan(gwas_result4,suggestiveline = F, cex.axis = 2,main=paste0("Global Test Association Manhattan Plot"))
