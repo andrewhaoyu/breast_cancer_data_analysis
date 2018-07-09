bc9fbea179e585ceb3f7c7b10cdb326652d6f48f
Zhang <andrew.haoyu@gmail.com>
2018-07-09 13:42:31 -0400

    merge

--- known_SNPs/known_SNPs_analysis_G_revised/additive_model_third_order/code/merge.R
+++ known_SNPs/known_SNPs_analysis_G_revised/additive_model_third_order/code/merge.R
@@ -123,53 +123,41 @@ generate_second_stage_parameter_names = function(tumor_characteristics){
              "Mixed Model global test p value",
              "Mixed Model global heterogeneity test p value"
              )
-             #"Mixed Model global test p value (baseline fixed)",
-             #"Mixed Model global heterogeneity test p value (baseline fixed)",
-             #"Mixed Model global test p value (baseline+ER fixed)",
-             #"Mixed Model global heterogeneity test p value (baseline+ER fixed)",
-             #"loglikelihood",
-             #"AIC")
+            
   return(result)
 }
 
 result <-  NULL
+p_mglobal <- NULL
+p_mheter <- NULL
 #first.stage <- NULL
 
 for(i1 in 1:178){
   print(i1)
   load(paste0("./known_SNPs/known_SNPs_analysis_G_revised/additive_model_third_order/result/heter_result_",i1,".Rdata"))
-  result <- rbind(result,c(heter.result[[1]],
-                    heter.result[[2]][1],
-                    heter.result[[3]][1]))
+  result <- rbind(result,heter.result[[1]])
+  p_mglobal <- c(p_mglobal,heter.result[[2]][1])
+  p_mheter <- c(p_mheter,heter.result[[3]][1])
+                    
   #first.stage <- rbind(first.stage,heter.result[[2]])
 }
-
+result <- cbind(result,p_mglobal,p_mheter)
 tumor.characteristics <- c("ER","PR","HER2","Grade")
 generate_second_stage_parameter_names(tumor.characteristics)
 colnames(result) <- generate_second_stage_parameter_names(tumor.characteristics)
 
 
 
-p.wald.assoc <- result[,13]
+p.wald.assoc <- as.vector(result[,13])
 p.wald.assoc[is.na(p.wald.assoc)] <- 0
 p.wald.assoc.adjust <- p.adjust(p.wald.assoc,method="BH")
-p.wald.heter <- result[,12]
+p.wald.heter <- result[,14]
 p.wald.heter.adjust <- p.adjust(p.wald.heter,method="BH")
-p.score.assoc <- result[,13]
-p.score.assoc.adjust <- p.adjust(p.score.assoc,method="BH")
-p.mixed.assoc.baseline.fixed <- result[,14]
-p.mixed.assoc.baseline.fixed.adjust <- 
-  p.adjust(p.mixed.assoc.baseline.fixed,
-           method="BH")
-p.mixed.heter.baseline.fixed <- result[,15]
-p.mixed.heter.baseline.fixed.adjust <- 
-  p.adjust(p.mixed.heter.baseline.fixed,
-           method="BH")
-p.mixed.assoc.baselineER.fixed <- result[,16]
+p.mixed.assoc.baselineER.fixed <- result[,15]
 p.mixed.assoc.baselineER.fixed.adjust <- 
   p.adjust(p.mixed.assoc.baselineER.fixed,
            method="BH")
-p.mixed.heter.baselineER.fixed <- result[,17]
+p.mixed.heter.baselineER.fixed <- result[,16]
 p.mixed.heter.baselineER.fixed.adjust <- 
   p.adjust(p.mixed.heter.baselineER.fixed,
            method="BH")
@@ -179,12 +167,6 @@ pvalue = data.frame(p.wald.assoc,
                     p.wald.assoc.adjust,
                     p.wald.heter,
                     p.wald.heter.adjust,
-                    p.score.assoc,
-                    p.score.assoc.adjust,
-                    p.mixed.assoc.baseline.fixed,
-                    p.mixed.assoc.baseline.fixed.adjust,
-                    p.mixed.heter.baseline.fixed,
-                    p.mixed.heter.baseline.fixed.adjust,
                     p.mixed.assoc.baselineER.fixed,
                     p.mixed.assoc.baselineER.fixed.adjust,
                     p.mixed.heter.baselineER.fixed,
@@ -194,41 +176,20 @@ colnames(pvalue) = c("Wald global test p value",
                      "Wald global test p value (BH adjust)",
                      "Wald global heterogneity test p value",
                      "Wald global heterogneity test p value (BH adjust)",
-                     "Score global test p value",
-                     "Score global test p value (BH adjust)",
-                     "Mixed Model global test p value (baseline fixed)",
-                     "Mixed Model global test p value (baseline fixed, BH adjust)",
-                     "Mixed Model global heterogeneity test p value (baseline fixed)",
-                     "Mixed Model global heterogeneity test p value (baseline fixed BH adjust)",
-                     "Mixed Model global test p value (baseline+ER fixed)",
-                     "Mixed Model global test p value (baseline+ER fixed, BH adjust)",
-                     "Mixed Model global heterogeneity test p value (baseline+ER fixed)",
-                     "Mixed Model global heterogeneity test p value (baseline+ER fixed BH adjust)")
+                     
+                     "Mixed Model global test p value",
+                     "Mixed Model global test p value",
+                     "Mixed Model global heterogeneity test p value",
+                     "Mixed Model global heterogeneity test p value")
 
-result <- result[,-c(11:17)]
+result <- result[,-c(13:16)]
 
 names.temp <- colnames(result)
 p.value.names <- colnames(pvalue)
-full.names <- c(names.temp[1:10],p.value.names,names.temp[11:12])
-result <- data.frame(result[1:10],pvalue,result[,11:12])
+full.names <- c(names.temp,p.value.names)
+result <- data.frame(result,pvalue)
 
 colnames(result) <- full.names
+write.csv(result,file = paste0("./known_SNPs/known_SNPs_analysis_G_revised/additive_model_third_order/result/known_third_order.csv"))
 
-
-
-
-
-
-
-
-
-
-
-
-#colnames(first.stage) <- generate_first_stage_parameter_names(tumor.characteristics,z.standard)
-
-#result <- data.frame(result)
-
-write.xlsx(result,file="./additive_model_G_new.xlsx",sheetName="additive_model_2nd_stage")
-#write.xlsx(first.stage,file="./additive_model.xlsx",sheetName="additive_model_1st_stage",append=T)
 
