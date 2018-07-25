08da6446192860a9d4aaf4b4dc16f7775e19b96e
Zhang <andrew.haoyu@gmail.com>
2018-07-25 15:25:54 -0400

    poly_high

--- simulation/power/code/poly_high.R
+++ simulation/power/code/poly_high.R
@@ -239,7 +239,8 @@ sc <- 3
 #result.list <- foreach(job.i = 1:2)%dopar%{
   set.seed(i1)
   s_times <- 5
-  sizes <- c(50000,100000)
+  #sizes <- c(50000,100000)
+  sizes <- c(25000)
   n.sizes <- length(sizes)
   # p_global_result <- rep(0,n.sizes*s_times)
   # p_mglobal_result <- rep(0,n.sizes*s_times)
