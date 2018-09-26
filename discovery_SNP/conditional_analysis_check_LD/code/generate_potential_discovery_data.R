c44a82f518282852d9a9a70ec17336c75e4f60cf
Zhang <andrew.haoyu@gmail.com>
2018-09-26 15:54:52 -0400

    generte_potential_discovery

--- discovery_SNP/conditional_analysis_check_LD/code/generate_potential_discovery_data.R
+++ discovery_SNP/conditional_analysis_check_LD/code/generate_potential_discovery_data.R
@@ -1,7 +1,7 @@
 ##############generate the discovery snps list
 load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_casecase/result/extract_result_shared.Rdata")
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/LD_pruning.result")
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_list.Rdata")
+# load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/LD_pruning.result")
+# load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_list.Rdata")
 load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p.Rdata")
 discovery_snp <- read.csv("/spin1/users/zhangh24/breast_cancer_data_analysis/data/SNP_infor_conditional_check.csv",header=T)
 discovery_snp <- discovery_snp[1:22,]
@@ -16,128 +16,82 @@ chr.pos.dis <- paste0(discovery_snp$CHR,":",
                        meta_result_shared_1p$position)
  idx.fil <- which(chr.pos.all%in%chr.pos.dis)
  length(chr.pos.all[idx.fil])
- meta_result_shared_1p[idx.fil,]
+ discovery_snp<-  meta_result_shared_1p[idx.fil,]
 # 
 
 
 
+ setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/")
+ snp.icogs.extract.id <- as.character(discovery_snp$SNP.ICOGS)
+ write.table(snp.icogs.extract.id,file = paste0("./discovery_SNP/result/extract_id_icog_discovery.txt"),quote = F,row.names=F)
+ snp.onco.extract.id <- as.character(discovery_snp$SNP.ONCO)
+ write.table(snp.onco.extract.id,file = paste0("./discovery_SNP/result/extract_id_onco_discovery.txt"),quote = F,row.names=F)
+ 
+ 
+ # /spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/result/extract_id_icog_discovery.txt
+ 
+ Filesdir <- "/gpfs/gsfs4/users/NC_BW/icogs_onco/genotype/imputed2/icogs_imputed/"
+ Files <- dir(Filesdir,pattern="icogs_merged_b1_12.",full.names=T)
+ Filesex <- dir(Filesdir,pattern="icogs_merged_b1_12.chr23",full.names=T)
+ idx.sex <- Files%in%Filesex
+ Files <- Files[!idx.sex]
+ library(gtools)
+ Files <- mixedsort(Files)
+ 
+ 
+ 
+ 
+ 
+ qctool.command <- rep("c",564)
+ qctool.command <- data.frame(qctool.command,stringsAsFactors=F)
+ 
+ 
+ for(i in 1:564){
+   geno.file <- Files[i]
+   temp <- paste0("/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g ",Files[i]," -incl-rsids /spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/result/extract_id_icog_discovery.txt -og /spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/result/discovery_icog",i,".txt")
+   qctool.command[i,1] <- temp
+   
+ }
+ 
+ 
+ write.table(qctool.command,file = paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/code/qc_extract_discovery_icog.sh"),col.names = F,row.names = F,quote=F)
+ 
+ 
+ 
+ 
+ 
+ Filesdir <- "/gpfs/gsfs4/users/NC_BW/icogs_onco/genotype/imputed2/onco_imputed"
+ Files <- dir(Filesdir,pattern="OncoArray_european_merged_b1_15.",full.names=T)
+ Filesex <- dir(Filesdir,pattern="OncoArray_european_merged_b1_15.chr23",full.names=T)
+ idx.sex <- Files%in%Filesex
+ Files <- Files[!idx.sex]
+ Files <- mixedsort(Files)
+ 
+ 
+ qctool.command <- rep("c",567)
+ qctool.command <- data.frame(qctool.command,stringsAsFactors=F)
+ 
+ 
+ for(i in 1:567){
+   geno.file <- Files[i]
+   temp <- paste0("/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g ",Files[i]," -incl-rsids /spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/result/extract_id_onco_discovery.txt -og /spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/result/discovery_onco",i,".txt")
+   qctool.command[i,1] <- temp
+   
+ }
+ 
+ 
+ write.table(qctool.command,file = paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/code/qc_extract_discovery_onco.sh"),col.names = F,row.names = F,quote=F)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 
 
 
-
-
-
-# length(chr.pos.dis)
-# duplicated(chr.pos.dis)
-# length(idx.fil)
-# 
-# which(chr.pos.dis%in%chr.pos.all[idx.fil]!=T)
-# 
-
-
-
-
-
-
-
-
-
-
-
-#########fixed effect model
-library(bc2)
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/LD_pruning.result")
-extract.result.fixed <- extract.list.ld.pruning.result
-idx <- which(is.na(extract.result.fixed$SNP.ICOGS)==T)
-
-extract.result.fixed.shared <- extract.result.fixed[-idx,]
-extract.result.onco.only <- extract.result.fixed[idx,]
-
-
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/extract_result_shared.Rdata")
-extract.result.onco.fixed <- extract.result
-
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/extract_result_shared.Rdata")
-extract.result.icog.fixed <- extract.result
-
-
-
-fill.match <- FillandMatch(extract.result.fixed.shared$SNP.ICOGS,extract.result.icog.fixed[[1]])
-idx.fil <- fill.match[[1]]
-idx.match <- fill.match[[2]]
-icog.name <- extract.result.icog.fixed[[1]][idx.fil][idx.match]
-all.equal(icog.name,extract.result.fixed.shared$SNP.ICOGS)
-discover.data.icog.fixed <- extract.result.icog.fixed[[2]][,idx.fil][,idx.match]
-colnames(discover.data.icog.fixed) <- icog.name
-write.csv(discover.data.icog.fixed,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/discover.data.icog.fixed.csv")
-
-save(discover.data.icog.fixed,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/discover.data.icog.fixed.Rdata")
-
-
-fill.match <- FillandMatch(extract.result.fixed.shared$SNP.ONCO,extract.result.onco.fixed[[1]])
-idx.fil <- fill.match[[1]]
-idx.match <- fill.match[[2]]
-onco.name <- extract.result.onco.fixed[[1]][idx.fil][idx.match]
-all.equal(onco.name,extract.result.fixed.shared$SNP.ONCO)
-discover.data.onco.fixed <- extract.result.onco.fixed[[2]][,idx.fil][,idx.match]
-colnames(discover.data.onco.fixed) <- onco.name
-write.csv(discover.data.onco.fixed,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/discover.data.onco.fixed.csv")
-
-save(discover.data.onco.fixed,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/discover.data.onco.fixed.Rdata")
-
-
-
-
-
-
-fill.match <- FillandMatch(extract.result.onco.only$SNP.ONCO,extract.result.onco.fixed[[1]])
-idx.fil <- fill.match[[1]]
-idx.match <- fill.match[[2]]
-onco.name <- extract.result.onco.fixed[[1]][idx.fil][idx.match]
-all.equal(onco.name,extract.result.onco.only$SNP.ONCO)
-discover.data.onco.fixed.oncoonly <- extract.result.onco.fixed[[2]][idx.fil][idx.match]
-colnames(discover.data.onco.fixed.oncoonly) <- onco.name
-write.csv(discover.data.onco.fixed.oncoonly,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/discover.data.onco.fixed.oncoonly.csv")
-
-save(discover.data.onco.fixed.oncoonly,file="/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ONCO/ERPRHER2GRADE_fixed_baseline/result/discover.data.onco.fixed.oncoonly.Rdata")
-
-
-
-
-
-
-
-idx <- which(extract.result.icog.fixed[[1]]%in%extract.result.fixed$SNP.ICOGS)
-icog.snp.name <- as.character(extract.result.icog.fixed[[1]])[idx]
-idx.match <- match(extract.result.fixed.shared$SNP.ICOGS,icog.snp.name)
-icog.snp.name <- 
-  icog.data.fixed <- extract.result.icog.fixed[[2]][,idx]
-
-all.equal(icog.snp.name,extract.result.fixed.shared$SNP.ICOGS)
-cbind(icog.snp.name,extract.result.fixed.shared$SNP.ICOGS)
-
-
-
-
-
-
-load("/spin1/users/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/ERPRHER2GRADE_casecase/result/LD_pruning_casecase.result")
-extract.result.random <- extract.list.ld.pruning.result
-
-
-idx <- which(extract.result.fixed$p.value<=5E-08)
-length(idx)
-jdx <- which(extract.result.random$p.value<=5E-08)
-length(jdx)
-
-var.name.fixed <- extract.result.fixed$var_name
-var.name.random <- extract.result.random$var_name
-var.name.comb <- c(as.character(var.name.fixed),as.character(var.name.random))
-var.name.comb <- unique(var.name.comb)
-length(var.name.fixed)
-length(var.name.random)
-length(var.name.comb)
 
 
