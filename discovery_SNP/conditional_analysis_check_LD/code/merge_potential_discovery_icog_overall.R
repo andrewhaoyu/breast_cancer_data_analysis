d42e25f568fd536bc2e0f7f356fae78dc5500283
Zhang <andrew.haoyu@gmail.com>
2018-10-03 12:09:40 -0400

    stndard_conditional_analysis

---
+++ discovery_SNP/conditional_analysis_check_LD/code/merge_potential_discovery_icog_overall.R
@@ -0,0 +1,134 @@
+load("/spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/conditional_analysis_check_LD/potential_discovery_snp.Rdata")
+
+
+setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/")
+snp.icogs.extract.id <- as.character(discovery_snp$SNP.ICOGS)
+snp.onco.extract.id <- as.character(discovery_snp$SNP.ONCO)
+
+setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/")
+
+n.raw <- 109713
+snpvalue <- rep(0,n.raw)
+subject.file <- "/gpfs/gsfs4/users/NC_BW/icogs_onco/genotype/imputed2/icogs_order.txt.gz"
+library(data.table)
+Icog.order <- read.table(gzfile(subject.file))
+setwd("/spin1/users/zhangh24/breast_cancer_data_analysis/")
+
+#data1 <- fread("./data/PRS_subtype_icgos_pheno_v10_euro.csv",header=T)
+data1 <- fread("./data/icogs_overall.csv",header=T)
+data1 <- as.data.frame(data1)
+y.pheno.mis1 <- cbind(data1$Behaviour1,data1$ER_status1,data1$PR_status1,data1$HER2_status1,data1$Grade1)
+colnames(y.pheno.mis1) = c("Behavior","ER","PR","HER2","Grade")
+#x.test.all.mis1 <- data1[,c(27:206)]
+SG_ID <- data1$SG_ID
+######################include covariates principal components 1-10
+x.covar.mis1 <- data1[,c(5:14)]
+######################imputed data in Icog, how many in the current analysis datas
+idx.fil <- Icog.order[,1]%in%SG_ID
+idx.match <- match(SG_ID,Icog.order[idx.fil,1])
+#Icog.order.match <- Icog.order[idx.fil,1][idx.match]
+library(bc2)
+
+
+extract.num <- length(snp.icogs.extract.id)
+snpid.result <- rep("c",extract.num)
+n.sub <- length(SG_ID)
+snpvalue.result <- matrix(0,n.sub,extract.num)
+
+
+#################merge the icog discovery snp data in a single file
+total <- 0
+
+for(i in 1:564){
+  
+  print(i)  
+  geno.file <- paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/conditional_analysis_check_LD/result/discovery_icog",i,".txt"
+  )
+  num <- as.numeric(system(paste0('cat ',geno.file,' | wc -l '),intern=T))
+  
+  if(num!=0){
+    
+    
+    con <- file(geno.file)
+    temp <- 0
+    open(con)
+    for(i in 1:num){
+      
+      oneLine <- readLines(con,n=1)
+      myVector <- strsplit(oneLine," ")
+      snpid <- as.character(myVector[[1]][3])
+      
+      
+      temp <- temp+1
+      snpid.result[temp+total] <- snpid
+      #snpvalue <- rep(0,n)
+      
+      
+      snppro <- as.numeric(unlist(myVector)[7:length(myVector[[1]])])
+      
+      snpvalue <- convert(snppro,n.raw)
+      snpvalue <- snpvalue[idx.fil][idx.match]
+      snpvalue.result[,temp+total] <- snpvalue
+      
+      
+    }
+    close(con)
+    
+    total <- total+num
+    
+    
+  }
+  
+  # if(is.null(result[[1]])==0){
+  #   temp <- length(result[[1]])
+  #   snpid.result[total+(1:temp)] <- result[[1]]
+  #   snpvalue.result[,total+(1:temp)] <- result[[2]]
+  #   total <- temp+total
+  # }
+}
+snpid.result <- snpid.result[1:total]
+snpvalue.result <- snpvalue.result[,1:total]
+
+
+
+
+idx.match <- match(snp.icogs.extract.id,snpid.result)
+snpid.result <- snpid.result[idx.match]
+all.equal(snpid.result,snp.icogs.extract.id)
+snpvalue.result <- snpvalue.result[,idx.match]
+extract.result <- list(snpid.result,snpvalue.result)
+colnames(snpvalue.result) <- snpid.result
+
+write.csv(snpvalue.result,file="/spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/conditional_analysis_check_LD/discovery_icog_data_overall.csv",row.names = F,quote=F)
+
+#data2 <- as.data.frame(fread("./data/Onco_euro_v10_10232017.csv",header=T))
+#data2 <- data2)
+
+#data2 <- fread("./data/PRS_subtype_Onco_euro_v10_08012018.csv",
+#               header=T)
+#data2 <- as.data.frame(data2)
+
+# ID <- data1[,1]
+# pc1 <- data1[5:14]
+# snpvalue1 <- cbind(data1[,27:203],snpvalue.result)
+# age <- data1[,204]
+# 
+# sig_snp_icog<- cbind(ID,y.pheno.mis1,
+#                      pc1,
+#                      snpvalue1,
+#                      age)
+# write.csv(sig_snp_icog,file = "/spin1/users/zhangh24/breast_cancer_data_analysis/data/sig_snp_icog.csv")
+# 
+# names1 <- colnames(data1)[27:203]
+# names2 <- colnames(data2)[27:204]
+# 
+# 
+# idx <- which(names2%in%names1==F)
+# idx.match <- match(names1,names2)
+# 
+
+
+
+
+
+
--- discovery_SNP/conditional_analysis_novel/code/conditional_analysis_standard_analysis.R
+++ discovery_SNP/conditional_analysis_novel/code/conditional_analysis_standard_analysis.R
@@ -7,7 +7,7 @@ setwd('/spin1/users/zhangh24/breast_cancer_data_analysis/')
 load("./discovery_SNP/conditional_analysis_check_LD/potential_discovery_snp.Rdata")
 
 
-discovery_snp_infor <- read.csv("/spin1/users/zhangh24/breast_cancer_data_analysis/data/SNP_infor_conditional_check.csv",header=T)
+discovery_snp_infor <- read.csv("/spin1/users/zhangh24/breast_cancer_data_analysis/data/SNP_infor_conditional_check.csv",header=T,stringsAsFactors = F)
 discovery_snp_infor <- discovery_snp_infor[1:20,]
 
 
@@ -19,52 +19,55 @@ discovery_snp_infor <- discovery_snp_infor[1:20,]
 
 library(bc2)
 data1 <- as.data.frame(fread("./data/icogs_overall.csv",header=T))
-discovery.snp.icog <- as.data.frame(fread("/spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/conditional_analysis_check_LD/discovery_icog_data.csv"))
+idx.case1 <- which(data1$Behaviour1==2|data1$Behaviour1==888)
+discovery.snp.icog <- as.data.frame(fread("/spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/conditional_analysis_check_LD/discovery_icog_data_overall.csv"))
+colnames(discovery.snp.icog)[5] <- "5:5369617"
 country1 <- as.factor(data1[,3])
+y.pheno.mis1 <- cbind(data1$Behaviour1,data1$ER_status1,data1$PR_status1,data1$HER2_status1,data1$Grade1)
+y.pheno.mis1[idx.case1,1] <- 1
+#y.pheno.mis2 <- cbind(data2$Behaviour1,data2$PR_status1,data2$ER_status1,data2$HER2_status1)
+colnames(y.pheno.mis1) = c("Behaviour","ER",
+                           "PR","HER2","Grade")
 
 snp.name <- colnames(discovery.snp.icog)[i1]
-########find the snp information
-idx <- which(discovery_snp$SNP.ICOGS==snp.name)
-snp.chr <- discovery_snp$CHR[idx]
-snp.pos <- discovery_snp$position[idx]
-########find the corresponding known snp name
-idx.infor <- which(discovery_snp_infor$CHR==snp.chr&
-                     discovery_snp_infor$Pos==snp.pos)
-if(idx.infor==15|idx.infor==19){
-  known.snp <- discovery_snp_infor[idx.infor,c(4,5)]
-  
+#################find the known snp information
+temp <- strsplit(snp.name,":")
+pos <- as.numeric(temp[[1]][2])
+idx.dis.infor <- which(discovery_snp_infor$Pos==pos)
+if(discovery_snp_infor[idx.dis.infor,5]==""){
+  near.known.snp.name <- discovery_snp_infor[idx.dis.infor,4]  
 }else{
-  known.snp <- discovery_snp_infor[idx.infor,4]  
+  near.known.snp.name <- discovery_snp_infor[idx.dis.infor,c(4,5)]  
 }
-idx.known <- which(colnames(data1)%in%known.snp)
+idx.known1 <- which(colnames(data1)%in%near.known.snp.name)
 
-x.covar1 <- cbind(data1[,c(5:14)],data1[,idx.known],country1)
+
+#idx.known <- which(colnames(data1)%in%known.snp)
+#x.covar1 <- cbind(data1[,c(6:15)],country1)
+x.covar1 <- cbind(data1[,c(6:15)],data1[,idx.known1],country1)
 gene1 <- discovery.snp.icog[,i1]
 
 
-data2 <- fread("./data/Onco_euro_v10_10232017.csv",header=T)
-discovery.snp.onco <- as.data.frame(fread("/spin1/users/zhangh24/breast_cancer_data_analysis/data/discovery_onco_data.csv"))
+data2 <- fread("./data/oncoarray_overall.csv",header=T)
+discovery.snp.onco <- as.data.frame(fread("/spin1/users/zhangh24/breast_cancer_data_analysis/discovery_SNP/conditional_analysis_check_LD/discovery_onco_data_overall.csv"))
 
 data2 <- as.data.frame(data2)
+idx.case2 <- which(data2$Behaviour1==2|data2$Behaviour1==888)
+data2$Behaviour1[idx.case] <- 1
 y.pheno.mis2 <- cbind(data2$Behaviour1,data2$ER_status1,data2$PR_status1,data2$HER2_status1,data2$Grade1)
+y.pheno.mis2[idx.case2,1] <- 1
 #y.pheno.mis2 <- cbind(data2$Behaviour1,data2$PR_status1,data2$ER_status1,data2$HER2_status1)
 colnames(y.pheno.mis2) = c("Behaviour","ER",
                            "PR","HER2","Grade")
 
 # x.test.all.mis2 <- data2[,c(27:203)]
-if(length(idx.check)==1){
-  idx.known <- which(colnames(data2)==check.data[idx.check+1,1])
-}else{
-  idx.known <- NULL
-  for(i in 1:length(idx.check)){
-    idx.temp <- which(colnames(data2)==check.data[idx.check[i]+1,1])
-    idx.known <- c(idx.known,idx.temp)
-  }
-}
+idx.known2 <- which(colnames(data2)%in%near.known.snp.name)
 country2 <- as.factor(data2[,4])
-x.covar2 <- cbind(data2[,c(5:14)],data2[,idx.known],country2)
-gene2 <- discovery.snp.onco[,check.id[i1,1]-10]
+#x.covar2 <- cbind(data2[,c(5:14)],country2)
+x.covar2 <- cbind(data2[,c(5:14)],data2[,idx.known2],country2)
+gene2 <- discovery.snp.onco[,i1]
 #age <- data2[,204]
+
 
 p.value.standard <- two_data_standard_anlysis(y.pheno.mis1,
                                               gene1,
@@ -73,10 +76,9 @@ p.value.standard <- two_data_standard_anlysis(y.pheno.mis1,
                                               gene2,
                                               x.covar2)
 
-
-result <- list(p.value.two.stage.model=p.value.two.stage.model
+result <- list(p.value.standard,snp.name
 )
 
-save(result,file=paste0("./discovery_SNP/conditional_analysis_novel/result/novel_conditional_result",i1,".Rdata"))
+save(result,file=paste0("./discovery_SNP/conditional_analysis_novel/result/standard_conditional_result",i1,".Rdata"))
 
 
