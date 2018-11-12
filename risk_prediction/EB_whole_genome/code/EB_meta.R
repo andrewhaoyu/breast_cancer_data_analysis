8fb90d8e1d756cd7d6a0064817d78aff83ecf045
Zhang <andrew.haoyu@gmail.com>
2018-11-12 14:10:01 -0500

    EB_meta

---
+++ risk_prediction/EB_whole_genome/code/EB_meta.R
@@ -0,0 +1,42 @@
+arg <- commandArgs(trailingOnly=T)
+i1 <- as.numeric(arg[[1]])
+library(bc2)
+library(bcutility)
+setwd('/spin1/users/zhangh24/breast_cancer_data_analysis/risk_prediction/')
+load('./intrinsic_subtypes_whole_genome/ICOG/result/meta_result_shared_1p.Rdata')
+meta_intrin <- meta_result_shared_1p
+load("./standard_whole_genome/ICOG/result/meta_result_shared_1p.Rdata")
+meta_stan <- meta_result_shared_1p
+
+stan_result <- meta_stan[,c(11,12,17)]
+intrin_result <- meta_intrin[,c(11:40,45)]
+
+size <- 1000
+start.end <- startend(n,size,i1)
+start <- start.end[1]
+end <- start.end[2]
+eblogodds_sub <- matrix(0,end-start+1,second.num)
+hetervar.sub <- rep(0,end-start+1)
+temp = 1
+second.num <- 5
+for(j in start:end){
+  print(j)
+  intri_odds <-  as.numeric(intrin_result[j,1:second.num])
+  intri_covar <-   matrix(
+    as.numeric(intrin_result[j,(second.num+1):
+              (second.num+second.num^2)]),
+    second.num,second.num)
+  hetervar.sub[j] <- HeterVarianceEstimate(
+    intri_odds,
+    intri_covar)
+  eblogodds_sub[temp,] <- ebestimate(intri_odds,
+                                 intri_covar,
+                                 stan_result[j,1],
+                                 hetervar.sub[j])
+  temp = temp+1
+}
+
+result_sub <- list(eblogodds_sub,
+                   hetervar.sub )
+
+save(result_sub,file=paste0("./EB_whole_genome/result/p_value_sub",i1,".Rdata"))
--- risk_prediction/EB_whole_genome/code/merge_standard_FTOP_intrin.r
+++ risk_prediction/EB_whole_genome/code/merge_standard_FTOP_intrin.r
@@ -8,3 +8,21 @@ meta_stan <- meta_result_shared_1p
 all.equal(meta_intrin$rs_id,meta_FTOP$rs_id)
 all.equal(meta_stan$rs_id,meta_FTOP$rs_id)
 meta_infor <- meta_intrin[,c(1:10,41:44)]
+stan_result <- meta_stan[,c(11,12,17)]
+colnames(stan_result) <- paste0("stan_",
+                                c("logodds",
+                                  "var",
+                                  "p"))
+FTOP_result <- meta_FTOP[,15]
+colnames(meta_FTOP) <- c("FTOP")
+intrin_result <- meta_intrin[,c(11:40,45)]
+colnames(intrin_result)[1:5] <- c("Luminial_A",
+                                  "Luminal_B",
+                                  "Luminal_B_HER2Neg",
+                                  "HER2_Enriched",
+                                  "TN")
+colnames(intrin_result)[31] <- "in_p"
+wholge_genome <- cbind(meta_infor,
+                       stan_result,
+                       FTOP_result,
+                       intrin_result)
