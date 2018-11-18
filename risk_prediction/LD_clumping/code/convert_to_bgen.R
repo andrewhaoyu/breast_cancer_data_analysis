0f57c970a839658b044b6b73956856e4e94677df
Zhang <andrew.haoyu@gmail.com>
2018-11-18 11:29:41 -0500

    convert_to_bgen

--- risk_prediction/LD_clumping/code/convert_to_bgen.R
+++ risk_prediction/LD_clumping/code/convert_to_bgen.R
@@ -5,9 +5,19 @@
 # Author: Haoyu Zhang
 #-------------------------------------------------------------------
 #prepare sample for all of the subjects
+names.subtypes <-  c("Luminal_A","Luminal_B",
+                     "Luminal_B_HER2Neg",
+                     "HER2Enriched",
+                     "TripleNeg")
 
+convert.to.bgen <- rep("c",length(names.subtypes))
+convert.to.bgen <- data.frame(convert.to.bgen,stringsAsFactors=F)
 
-convert.to.bgen <- "/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g /spin1/users/zhangh24/BCAC/impute_onco/onco_all.gz -s /spin1/users/zhangh24/BCAC/sample_onco.txt -incl-samples /spin1/users/zhangh24/BCAC/test_sample_Luminal_A.txt -incl-rsids /spin1/users/zhangh24/BCAC/impute_onco/onco_1p_shared_id.txt -og /spin1/users/zhangh24/BCAC/impute_test_bgen/Luminal_A_test.bgen"
+for(i in 1:length(names.subtypes)){
+  convert.to.bgen.code <- paste0("/spin1/users/zhangh24/qctool_v1.4-linux-x86_64/qctool -g /spin1/users/zhangh24/BCAC/impute_onco/onco_all.gz -s /spin1/users/zhangh24/BCAC/sample_onco.txt -incl-samples /spin1/users/zhangh24/BCAC/test_sample_",names.subtypes[i],".txt -incl-rsids /spin1/users/zhangh24/BCAC/impute_onco/onco_1p_shared_id.txt -og /spin1/users/zhangh24/BCAC/impute_test_bgen/",names.subtypes[i],"_test.bgen")
+  convert.to.bgen[i,1] <- convert.to.bgen.code
+}
+
 
 write.table(convert.to.bgen,file="/spin1/users/zhangh24/breast_cancer_data_analysis/risk_prediction/LD_clumping/code/convert_to_bgen",
             row.names=F,col.names = F,
