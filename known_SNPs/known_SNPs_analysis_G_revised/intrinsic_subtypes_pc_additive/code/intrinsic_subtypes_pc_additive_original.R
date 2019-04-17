6a0822f49c997261bebf94f89e5505308756bc81
Zhang <andrew.haoyu@gmail.com>
2019-04-17 15:39:46 -0400

    intrinsic subtypes

--- known_SNPs/known_SNPs_analysis_G_revised/intrinsic_subtypes_pc_additive/code/intrinsic_subtypes_pc_additive_original.R
+++ known_SNPs/known_SNPs_analysis_G_revised/intrinsic_subtypes_pc_additive/code/intrinsic_subtypes_pc_additive_original.R
@@ -67,7 +67,11 @@ if(i1<=177){
   ###pc1-10 and age
   x.covar.mis1 <- data1[,c(5:14)]
   
-  
+  idx.control <- which(y.pheno.mis1[,1]==0)
+  maf <- sum(x.test.all.mis1[idx.control,i1])/(2*length(idx.control))
+  if(maf>=0.5){
+    x.test.all.mis1[,i1] < 2 - x.test.all.mis1[,i1]
+  }
   
   x.all.mis1 <- as.matrix(cbind(x.test.all.mis1[,i1],x.covar.mis1))
   colnames(x.all.mis1)[1] <- "gene"
