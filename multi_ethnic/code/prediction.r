73e1e9dec8941c74f3c9ece1ae2e3d5f26092f45
Zhang <andrew.haoyu@gmail.com>
2019-05-06 17:39:15 -0400

    predict

--- multi_ethnic/code/prediction.r
+++ multi_ethnic/code/prediction.r
@@ -6,7 +6,7 @@ args <- commandArgs(trailingOnly = T)
 pop.ind <- as.numeric(args[[1]])
 setwd('/spin1/users/zhangh24/breast_cancer_data_analysis')
 load(paste0("./multi_ethnic/result/y_",1))
-
+y_all = y
 
 
 #find all genotypes data files cutpoint
@@ -27,9 +27,9 @@ load(paste0("./multi_ethnic/result/pruned_geno/beta_all_",1,".Rdata"))
 
 
 #pop.ind population indicator 1 EUR, 2 AFR, 3 LAC
-LDP <- function(y,beta.train,beta.test,beta.vad,p.train,
+LDP2 <- function(y_all,beta.train,beta.test,beta.vad,p.train,
                 p.thr,pop.ind){
-  y = y[[pop.ind]]
+  y = y_all[[pop.ind]]
   n.sub <- length(y)
   n.train <- n.sub*10/12
   n.test <- n.sub/12
@@ -42,30 +42,63 @@ LDP <- function(y,beta.train,beta.test,beta.vad,p.train,
   prs.mat <- matrix(0,n.test+n.vad,length(p.thr))
   
   
-  # n.snp <- 0
-  # for(i in 1:500){
-  #   load(paste0("./multi_ethnic/result/pruned_geno/geno_",i))
-  #   temp <- ncol(genotype[[pop.ind]])
-  #   for(j in 1:temp){
-  #     jdx <- which(p.train[n.snp+temp]<=p.thr)
-  #       if(length(jdx)!=0){
-  #         prs.temp <- genotype[[pop.ind]][,j]*beta.train[n.snp+temp]
-  #         prs.mat[,jdx] <- prs.mat[,jdx]+
-  #           prs.temp[n.train+(1:(n.test+n.vad))]
-  #       }
-  #   }
-  #   n.snp <- n.snp+temp
-  # }
+  n.snp <- 0
+  for(i in 1:500){
+    print(i)
+    load(paste0("./multi_ethnic/result/pruned_geno/geno_",i))
+    temp <- ncol(genotype[[pop.ind]])
+    for(j in 1:temp){
+      jdx <- which(p.train[n.snp+j]<=p.thr)
+        if(length(jdx)!=0){
+          prs.temp <- genotype[[pop.ind]][,j]*beta.train[n.snp+temp]
+          prs.mat[,jdx] <- prs.mat[,jdx]+
+          prs.temp[n.train+(1:(n.test+n.vad))]
+        }
+    }
+    n.snp <- n.snp+temp
+  }
 
+  for(k in 1:length(p.thr)){
+    idx <- which(p.train<=p.thr[k])
+    if(length(idx)==0){
+      n.snp.sec[k] <- 0
+      prop[k] <- 0
+      r2.test[k] <- 0
+      r2.vad[k] <- 0
+    }else{
+      n.snp.sec[k] <- length(idx)
+      cau.snp <- c(1:4000,4000+c(1:1000)*pop.ind)
+      prop[k] <- sum(idx%in%cau.snp)/length(idx)
+      prs.test <- prs.mat[(1:n.test),k]
+      prs.vad <- prs.mat[n.test+(1:n.vad),k]
+      model1 <- lm(y.test~prs.test-1)
+      r2.test[k] <- summary(model1)$adj.r.squared
+      model2 <- lm(y.vad~prs.vad-1)
+      r2.vad[k] <- summary(model2)$adj.r.squared
+    }
+  }
 
-
+  result <- list(n.snp.sec,prop,
+                 r2.test,r2.vad,
+                 prs.mat)
+  return(result)
+}  
   
   
   
-  
-  
-  
-  
+LDP <- function(y_all,beta.train,beta.test,beta.vad,p.train,
+                p.thr,pop.ind){
+  y = y_all[[pop.ind]]
+  n.sub <- length(y)
+  n.train <- n.sub*10/12
+  n.test <- n.sub/12
+  n.vad <- n.sub/12
+  y.test <- y[n.train+(1:n.test)]
+  y.vad <- y[(n.train+n.test)+(1:n.vad)]
+  r2.vad <- r2.test <- rep(0,length(p.thr))
+  prop <- rep(0,length(p.thr))
+  n.snp.sec <- rep(0,length(p.thr))
+  prs.mat <- matrix(0,n.test+n.vad,length(p.thr))
   
   for(k in 1:length(p.thr)){
     #print(k)
@@ -113,14 +146,16 @@ LDP <- function(y,beta.train,beta.test,beta.vad,p.train,
       r2.vad[k] <- summary(model2)$adj.r.squared
       prs.mat[,k] <- c(prs.test,prs.vad)
     }
-  
-    }
+    
+  }
   result <- list(n.snp.sec,prop,
                  r2.test,r2.vad,
                  prs.mat)
   return(result)
   
 }
+
+
   
 
 
