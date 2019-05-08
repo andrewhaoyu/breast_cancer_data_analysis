fdc3c1efe1b703743d14aae96bcf3e726ca2d2aa
Zhang <andrew.haoyu@gmail.com>
2019-05-08 09:05:10 -0400

    fit_regression

--- multi_ethnic/code/fit_regression.R
+++ multi_ethnic/code/fit_regression.R
@@ -13,9 +13,49 @@
 #Genetic correlation for the between EUR and LAC is 0.6
 #Genetic correlation for the between LAC and AFR is 0.6
 setwd('/spin1/users/zhangh24/breast_cancer_data_analysis')
+library(RcppArmadillo)
+
+FitLinearmodel <- function(y,x){
+  model <- fastLm(X=cbind(1,x),y=y)
+  if(is.na(coef(model)[2])){
+    result <- c(0,1,1)
+  }else{
+    result <- coef(summary(model))[2,c(1,2,4)]  
+  }
+  return(result)
+}
+Fitmodelall <- function(y,G,ind){
+  result.train <- NULL
+  result.test <- NULL
+  result.vad <- NULL
+  #i from 1 to 3 represent EUR, AFR, LAC
+  for(i in 1:3){
+    
+    n.sub <- nrow(y[[i]])
+    n.train <- n.sub*10/12
+    n.test <- n.sub/12
+    n.vad <- n.sub/12
+    result.i <- NULL
+    result.temp <- FitLinearmodel(y[[i]][1:n.train],
+                                  G[[i]][1:n.train,ind])
+    result.train <- c(result.train,result.temp)
+    result.temp <- FitLinearmodel(y[[i]][(1:n.test)+n.train],
+                                  G[[i]][(1:n.test)+n.train,ind])
+    result.test <- c(result.test,result.temp)
+    result.temp <- FitLinearmodel(y[[i]][(1:n.vad)+n.test+n.train],
+                                  G[[i]][(1:n.vad)+n.test+n.train,ind])
+    result.vad <- c(result.vad,result.temp)
+  }
+  return(list(result.train,result.test,result.vad))
+  
+  
+}
+
 arg <- commandArgs(trailingOnly=T)
 i1 <- as.numeric(arg[[1]])
 i2 <- as.numeric(arg[[2]])
+print(i1)
+print(i2)
 #since first file is large, we use double args to parrallel the jobs
 if(i1 ==1){
   load(paste0("./multi_ethnic/result/pruned_geno/geno_",i1))
@@ -35,43 +75,6 @@ if(i1 ==1){
     colnames(beta_summary_train) <- c("beta_EUR","sd_EUR","p_EUR",
                                       "beta_AFR","sd_AFR","p_AFR", "beta_LAC","sd_LAC","p_LAC")
     colnames(beta_summary_vad) = colnames(beta_summary_test) = colnames(beta_summary_train)
-    library(RcppArmadillo)
-    
-    FitLinearmodel <- function(y,x){
-      model <- fastLm(X=cbind(1,x),y=y)
-      if(is.na(coef(model)[2])){
-        result <- c(0,1,1)
-      }else{
-        result <- coef(summary(model))[2,c(1,2,4)]  
-      }
-      return(result)
-    }
-    Fitmodelall <- function(y,G,ind){
-      result.train <- NULL
-      result.test <- NULL
-      result.vad <- NULL
-      #i from 1 to 3 represent EUR, AFR, LAC
-      for(i in 1:3){
-        
-        n.sub <- nrow(y[[i]])
-        n.train <- n.sub*10/12
-        n.test <- n.sub/12
-        n.vad <- n.sub/12
-        result.i <- NULL
-        result.temp <- FitLinearmodel(y[[i]][1:n.train],
-                                      G[[i]][1:n.train,ind])
-        result.train <- c(result.train,result.temp)
-        result.temp <- FitLinearmodel(y[[i]][(1:n.test)+n.train],
-                                      G[[i]][(1:n.test)+n.train,ind])
-        result.test <- c(result.test,result.temp)
-        result.temp <- FitLinearmodel(y[[i]][(1:n.vad)+n.test+n.train],
-                                      G[[i]][(1:n.vad)+n.test+n.train,ind])
-        result.vad <- c(result.vad,result.temp)
-      }
-      return(list(result.train,result.test,result.vad))
-      
-      
-    }
     
     for(i in 1:n.snp){
       if(i%%100==0){
@@ -108,42 +111,7 @@ if(i1 ==1){
     colnames(beta_summary_vad) = colnames(beta_summary_test) = colnames(beta_summary_train)
     library(RcppArmadillo)
     
-    FitLinearmodel <- function(y,x){
-      model <- fastLm(X=cbind(1,x),y=y)
-      if(is.na(coef(model)[2])){
-        result <- c(0,1,1)
-      }else{
-        result <- coef(summary(model))[2,c(1,2,4)]  
-      }
-      return(result)
-    }
-    Fitmodelall <- function(y,G,ind){
-      result.train <- NULL
-      result.test <- NULL
-      result.vad <- NULL
-      #i from 1 to 3 represent EUR, AFR, LAC
-      for(i in 1:3){
-        
-        n.sub <- nrow(y[[i]])
-        n.train <- n.sub*10/12
-        n.test <- n.sub/12
-        n.vad <- n.sub/12
-        result.i <- NULL
-        result.temp <- FitLinearmodel(y[[i]][1:n.train],
-                                      G[[i]][1:n.train,ind])
-        result.train <- c(result.train,result.temp)
-        result.temp <- FitLinearmodel(y[[i]][(1:n.test)+n.train],
-                                      G[[i]][(1:n.test)+n.train,ind])
-        result.test <- c(result.test,result.temp)
-        result.temp <- FitLinearmodel(y[[i]][(1:n.vad)+n.test+n.train],
-                                      G[[i]][(1:n.vad)+n.test+n.train,ind])
-        result.vad <- c(result.vad,result.temp)
-      }
-      return(list(result.train,result.test,result.vad))
-      
-      
-    }
-    
+  
     for(i in 1:n.snp){
       if(i%%100==0){
         print(i)
--- multi_ethnic/code/merge_summary_level_stat.R
+++ multi_ethnic/code/merge_summary_level_stat.R
@@ -1,38 +1,44 @@
 #Goal: merge summary level statistics
+#get the total number of snps
 n.snp <- 0
+k <- 1
 for(i1 in 1:500){
   if(i1%%50==0){
     print(i1)  
   }
-  load(paste0("./multi_ethnic/result/pruned_geno/beta_estimate_",i1,".Rdata"))
+  load(paste0("./multi_ethnic/result/pruned_geno/beta_estimate_",k,"_",i1,".Rdata"))
   temp <- nrow(beta_result[[1]])
   n.snp = n.snp +temp
 }
 
-beta_summary_train <- matrix(0,n.snp,9)
-beta_summary_test <- matrix(0,n.snp,9)
-beta_summary_vad <- matrix(0,n.snp,9)
-
-
-colnames(beta_summary_train) <- c("beta_EUR","sd_EUR","p_EUR",
-                                  "beta_AFR","sd_AFR","p_AFR", "beta_LAC","sd_LAC","p_LAC")
-n.snp <- 0
-for(i1 in 1:500){
-  if(i1%%50==0){
-    print(i1)  
+for(k in 1:100){
+  
+  
+  beta_summary_train <- matrix(0,n.snp,9)
+  beta_summary_test <- matrix(0,n.snp,9)
+  beta_summary_vad <- matrix(0,n.snp,9)
+  
+  
+  colnames(beta_summary_train) <- c("beta_EUR","sd_EUR","p_EUR",
+                                    "beta_AFR","sd_AFR","p_AFR", "beta_LAC","sd_LAC","p_LAC")
+  n.snp <- 0
+  for(i1 in 1:500){
+    if(i1%%50==0){
+      print(i1)  
+    }
+    load(paste0("./multi_ethnic/result/pruned_geno/beta_estimate_",i1,".Rdata"))
+    temp <- nrow(beta_result[[1]])
+    beta_summary_train[n.snp+(1:temp),] <- beta_result[[1]]
+    beta_summary_test[n.snp+(1:temp),] <- beta_result[[2]]
+    beta_summary_vad[n.snp+(1:temp),] <- beta_result[[3]]
+    n.snp = n.snp +temp
   }
-  load(paste0("./multi_ethnic/result/pruned_geno/beta_estimate_",i1,".Rdata"))
-  temp <- nrow(beta_result[[1]])
-  beta_summary_train[n.snp+(1:temp),] <- beta_result[[1]]
-  beta_summary_test[n.snp+(1:temp),] <- beta_result[[2]]
-  beta_summary_vad[n.snp+(1:temp),] <- beta_result[[3]]
-  n.snp = n.snp +temp
-}
-
-beta_result <- list(beta_summary_train,
-                    beta_summary_test,
-                    beta_summary_vad)
-save(beta_result,file = paste0("./multi_ethnic/result/pruned_geno/beta_all_",1,".Rdata"))
+  
+  beta_result <- list(beta_summary_train,
+                      beta_summary_test,
+                      beta_summary_vad)
+  save(beta_result,file = paste0("./multi_ethnic/result/pruned_geno/beta_all_",k,".Rdata"))
+  }
 
 
 
