5cd89b276578542acf9178d677ef4fbcba95b0d4
Zhang <andrew.haoyu@gmail.com>
2019-05-16 23:57:35 -0400

    generate phenotype

--- multi_ethnic/code/generate_phenotype.R
+++ multi_ethnic/code/generate_phenotype.R
@@ -14,6 +14,7 @@
 #Genetic correlation for the between LAC and AFR is 0.6
 args = commandArgs(trailingOnly = T)
 i1 = as.numeric(args[[1]])
+i2 = as.numeric(args[[2]])
 setwd('/spin1/users/zhangh24/breast_cancer_data_analysis')
 load(paste0("./multi_ethnic/result/pruned_geno/geno_",1))
 
@@ -33,15 +34,19 @@ GenSigma <- function(her1,her2,her3,
           
 }
 
+gr.set <- list(c(0.4,0.6,0.6),
+               c(0.6,0.4,0.6))
+
 n.shared <- 4000
 n.nonshare <- 1000
 n.cau <- n.shared+n.nonshare
 her.EUR <- 0.5
 her.AFR <- 0.5
 her.LAC <- 0.5
-gr.EUR.AFR <- 0.4
-gr.EUR.LAC <- 0.6
-gr.LAC.AFR <- 0.6
+gr.vec <- gr.set[[i2]]
+gr.EUR.AFR <- gr.vec[1]
+gr.EUR.LAC <- gr.vec[2]
+gr.LAC.AFR <- gr.vec[3]
 Sigma <- GenSigma(her.EUR/n.cau,
                   her.AFR/n.cau,
                   her.LAC/n.cau,
@@ -61,16 +66,16 @@ library(mvtnorm)
 #######################################
 # set.seed(666)
 # beta_shared <- rmvnorm(n.shared,c(0,0,0),
-#                        sigma=Sigma)
-# #non shared beta coefcients
-# all_nonshare <- 3*n.nonshare
-# beta_nonshared <- matrix(0,all_nonshare,3)
-# beta_nonshared[1:n.nonshare,1] <- rnorm(n.nonshare,0,sd=sqrt(her.EUR/n.cau))
-# beta_nonshared[(n.nonshare+1):(n.nonshare*2),2] <- rnorm(n.nonshare,0,sd=sqrt(her.AFR/n.cau))
-# beta_nonshared[(n.nonshare*2+1):(all_nonshare),3] <- rnorm(n.nonshare,0,sd=sqrt(her.LAC/n.cau))
-# beta <- rbind(beta_shared,beta_nonshared)
-# #save effect size for the causal SNPs
-# save(beta,file = "./multi_ethnic/result/pruned_geno/effect_size.Rdata")
+#                         sigma=Sigma)
+# # #non shared beta coefcients
+#  all_nonshare <- 3*n.nonshare
+#  beta_nonshared <- matrix(0,all_nonshare,3)
+#  beta_nonshared[1:n.nonshare,1] <- rnorm(n.nonshare,0,sd=sqrt(her.EUR/n.cau))
+#  beta_nonshared[(n.nonshare+1):(n.nonshare*2),2] <- rnorm(n.nonshare,0,sd=sqrt(her.AFR/n.cau))
+#  beta_nonshared[(n.nonshare*2+1):(all_nonshare),3] <- rnorm(n.nonshare,0,sd=sqrt(her.LAC/n.cau))
+#  beta <- rbind(beta_shared,beta_nonshared)
+# # #save effect size for the causal SNPs
+#  save(beta,file = paste0("./multi_ethnic/result/pruned_geno/effect_size_",i2,".Rdata"))
 ############################################
 
 
@@ -84,7 +89,7 @@ MAF.LAC <- pruned.snp.permu$MAF.LAC
 all_nonshare <- 3*n.nonshare
 #n.all are all the SNPs in the three populations with non-zero effects
 n.all <- n.shared+all_nonshare
-load("./multi_ethnic/result/pruned_geno/effect_size.Rdata")
+load(paste0("./multi_ethnic/result/pruned_geno/effect_size_",i2,".Rdata"))
 b <- beta
 b[,1] <- beta[,1]/sqrt(2*(MAF.EUR*(1-MAF.EUR))[1:n.all])
 b[,2] <- beta[,2]/sqrt(2*(MAF.AFR*(1-MAF.AFR))[1:n.all])
@@ -105,7 +110,7 @@ set.seed(i1)
     y_LAC <- genotype[[3]]%*%b[,3]+rnorm(n.LAC,0,sd=sqrt(1-her.LAC))
     
     y <- list(y_EUR,y_AFR,y_LAC)
-    save(y,file = paste0("./multi_ethnic/result/y_",i1))
+    save(y,file = paste0("./multi_ethnic/result/y_",i1,"_",i2))
     
  
     
