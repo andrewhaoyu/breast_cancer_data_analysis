24184a3d44c194a8e247b511629a730358f9457b
Zhang <andrew.haoyu@gmail.com>
2019-06-12 14:01:52 -0400

    genearte_all_manhattan

--- discovery_SNP/code/generate_all_mahanttan_plot_jh.R
+++ discovery_SNP/code/generate_all_mahanttan_plot_jh.R
@@ -42,8 +42,8 @@ if(i1 ==1){
   
   fine_mapping <- read.csv("./data/filter_regions_standard.csv",header= T)
   gwas_result_filter <- FilterSNP(gwas_result,fine_mapping) 
-  dim(gwas_result)
-  idx <- sample(c(1:10760767),100000)
+ # dim(gwas_result)
+  #idx <- sample(c(1:10760767),100000)
   if(i2==1){
     png(paste0("./discovery_SNP/result/manhattan_plot/man_standard.png"),width = 7.635,height =4.7175,units = "in",res = 300,type="cairo")
     manhattan(gwas_result, col= c("blue4", "orange3"),
@@ -75,7 +75,7 @@ if(i1 ==1){
   mtop <- meta_result_shared_1p
   mtop.p <- mtop$p.value
   subtypes.p <- apply(cbind(ftop.p,mtop.p),1,min)
-  ftop$subtypes.p <- ftop$p.value
+  ftop$subtypes.p <- subtypes.p
   ftop =  ftop %>% 
     mutate(subtypes.p.new = ifelse((is.nan(subtypes.p)==T)|(subtypes.p==0),1E-20,subtypes.p))
   subtypes_gwas_result <- ftop %>%
@@ -168,6 +168,121 @@ if(i1 ==1){
   }else if(i2==4){
     png(paste0("./discovery_SNP/result/manhattan_plot/qq_cimba_filter.png"),width = 4.7175,height =4.7175,units = "in",res = 300,type="cairo")
     qq(cimba_result_filter$P)
+    dev.off()
+  }
+}else if(i1==4){
+  #load("./whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p.Rdata")
+  meta_result_shared_1p <- as.data.frame(fread("/dcl01/chatterj/data/hzhang1/breast_intrinsic/meta_result_shared_1p_fixed.txt",header=T))
+  ftop <- meta_result_shared_1p
+  ftop.p <- ftop$p.value
+  subtypes.p <- ftop.p
+  ftop$subtypes.p <- ftop$p.value
+  ftop =  ftop %>% 
+    mutate(subtypes.p.new = ifelse((is.nan(subtypes.p)==T)|(subtypes.p==0),1E-20,subtypes.p))
+  subtypes_gwas_result <- ftop %>%
+    select(rs_id,CHR,position,subtypes.p.new)
+  colnames(subtypes_gwas_result) <- c("SNP",
+                                      "CHR",
+                                      "BP",
+                                      "P")
+  fine_mapping <- read.csv("./data/filter_regions_subtypes.csv",header= T)
+  subtypes_gwas_result_filter <- FilterSNP(subtypes_gwas_result,fine_mapping) 
+  
+  if(i2==1){
+    png(paste0("./discovery_SNP/result/manhattan_plot/man_fixed_subtypes.png"),width = 7.635,height =4.7175,units = "in",res = 300,type="cairo")
+    manhattan(subtypes_gwas_result, col= c("blue4", "orange3"),
+              cex = 0.6,suggestiveline = F)
+    dev.off()
+    
+  }else if(i2==2){
+    png(paste0("./discovery_SNP/result/manhattan_plot/man_fixed_subtypes_filter.png"),width = 7.635,height =4.7175,units = "in",res = 300,type="cairo")
+    manhattan(subtypes_gwas_result_filter, col= c("blue4", "orange3"),
+              cex = 0.6,suggestiveline = F)
+    dev.off()
+  }else if(i2==3){
+    png(paste0("./discovery_SNP/result/manhattan_plot/qq_fixed_subtypes.png"),width = 4.7175,height =4.7175,units = "in",res = 300,type="cairo")
+    qq(subtypes_gwas_result$P)
+    dev.off()
+  }else if(i2==4){
+    png(paste0("./discovery_SNP/result/manhattan_plot/qq_fixed_subtypes_filter.png"),width = 4.7175,height =4.7175,units = "in",res = 300,type="cairo")
+    qq(subtypes_gwas_result_filter$P)
+    dev.off()
+  }
+}else if(i1==4){
+  #load("./whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p.Rdata")
+  meta_result_shared_1p <- as.data.frame(fread("/dcl01/chatterj/data/hzhang1/breast_intrinsic/meta_result_shared_1p_fixed.txt",header=T))
+  ftop <- meta_result_shared_1p
+  ftop.p <- ftop$p.value
+  subtypes.p <- ftop.p
+  ftop$subtypes.p <- ftop$p.value
+  ftop =  ftop %>% 
+    mutate(subtypes.p.new = ifelse((is.nan(subtypes.p)==T)|(subtypes.p==0),1E-20,subtypes.p))
+  subtypes_gwas_result <- ftop %>%
+    select(rs_id,CHR,position,subtypes.p.new)
+  colnames(subtypes_gwas_result) <- c("SNP",
+                                      "CHR",
+                                      "BP",
+                                      "P")
+  fine_mapping <- read.csv("./data/filter_regions_subtypes.csv",header= T)
+  subtypes_gwas_result_filter <- FilterSNP(subtypes_gwas_result,fine_mapping) 
+  
+  if(i2==1){
+    png(paste0("./discovery_SNP/result/manhattan_plot/man_fixed_subtypes.png"),width = 7.635,height =4.7175,units = "in",res = 300,type="cairo")
+    manhattan(subtypes_gwas_result, col= c("blue4", "orange3"),
+              cex = 0.6,suggestiveline = F)
+    dev.off()
+    
+  }else if(i2==2){
+    png(paste0("./discovery_SNP/result/manhattan_plot/man_fixed_subtypes_filter.png"),width = 7.635,height =4.7175,units = "in",res = 300,type="cairo")
+    manhattan(subtypes_gwas_result_filter, col= c("blue4", "orange3"),
+              cex = 0.6,suggestiveline = F)
+    dev.off()
+  }else if(i2==3){
+    png(paste0("./discovery_SNP/result/manhattan_plot/qq_fixed_subtypes.png"),width = 4.7175,height =4.7175,units = "in",res = 300,type="cairo")
+    qq(subtypes_gwas_result$P)
+    dev.off()
+  }else if(i2==4){
+    png(paste0("./discovery_SNP/result/manhattan_plot/qq_fixed_subtypes_filter.png"),width = 4.7175,height =4.7175,units = "in",res = 300,type="cairo")
+    qq(subtypes_gwas_result_filter$P)
+    dev.off()
+  }
+}else if(i1==5){
+  #load("./whole_genome_age/ICOG/ERPRHER2GRADE_fixed_baseline/result/meta_result_shared_1p.Rdata")
+  meta_result_shared_1p <- as.data.frame(fread("/dcl01/chatterj/data/hzhang1/breast_intrinsic/meta_result_shared_1p_mixed.txt",header=T))
+  mtop <- meta_result_shared_1p
+  mtop.p <- mtop$p.value
+  #just for easy coding, put mtop as ftop
+  ftop = mtop
+  ftop$subtypes.p <- ftop$p.value
+  ftop =  ftop %>% 
+    mutate(subtypes.p.new = ifelse((is.nan(subtypes.p)==T)|(subtypes.p==0),1E-20,subtypes.p))
+  subtypes_gwas_result <- ftop %>%
+    select(rs_id,CHR,position,subtypes.p.new)
+  colnames(subtypes_gwas_result) <- c("SNP",
+                                      "CHR",
+                                      "BP",
+                                      "P")
+  fine_mapping <- read.csv("./data/filter_regions_subtypes.csv",header= T)
+  subtypes_gwas_result_filter <- FilterSNP(subtypes_gwas_result,fine_mapping) 
+  
+  if(i2==1){
+    png(paste0("./discovery_SNP/result/manhattan_plot/man_subtypes.png"),width = 7.635,height =4.7175,units = "in",res = 300,type="cairo")
+    manhattan(subtypes_gwas_result, col= c("blue4", "orange3"),
+              cex = 0.6,suggestiveline = F)
+    dev.off()
+    
+  }else if(i2==2){
+    png(paste0("./discovery_SNP/result/manhattan_plot/man_subtypes_filter.png"),width = 7.635,height =4.7175,units = "in",res = 300,type="cairo")
+    manhattan(subtypes_gwas_result_filter, col= c("blue4", "orange3"),
+              cex = 0.6,suggestiveline = F)
+    dev.off()
+  }else if(i2==3){
+    png(paste0("./discovery_SNP/result/manhattan_plot/qq_subtypes.png"),width = 4.7175,height =4.7175,units = "in",res = 300,type="cairo")
+    qq(subtypes_gwas_result$P)
+    dev.off()
+  }else if(i2==4){
+    png(paste0("./discovery_SNP/result/manhattan_plot/qq_subtypes_filter.png"),width = 4.7175,height =4.7175,units = "in",res = 300,type="cairo")
+    qq(subtypes_gwas_result_filter$P)
     dev.off()
   }
 }
