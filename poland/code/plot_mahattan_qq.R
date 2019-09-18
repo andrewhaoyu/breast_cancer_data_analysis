e2dad448e3b5d1a75b12b0959866473656cb4cfb
Zhang <andrew.haoyu@gmail.com>
2019-09-18 11:29:21 -0400

    plot manhattan

--- poland/code/plot_mahattan_qq.R
+++ poland/code/plot_mahattan_qq.R
@@ -6,6 +6,7 @@ load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_geno
 load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_casecase_5p.Rdata")
 load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_standard_5p.Rdata")
 if(i1 == 1){
+  #fixed effect two-stage model
   idx <- which(onco_result_standard_5p$p_value1<=5E-08)
   onco_result_standard_5p[idx,]
   idx2 <- which(onco_result_casecase_5p$P<=5E-08)
@@ -22,6 +23,7 @@ if(i1 == 1){
   dev.off()
   
 }else if(i1==2){
+  #mixed effect two stage model
   gwas_result2 <- data.frame(SNP=as.character(onco_result_casecase_5p$rs_id),CHR=onco_result_casecase_5p$CHR,BP=onco_result_casecase_5p$position,
                              P=onco_result_casecase_5p$P,stringsAsFactors =F)
   
@@ -33,6 +35,7 @@ if(i1 == 1){
   dev.off()
   
 }else if(i1==3){
+  #standard logistic regression
   gwas_result3 <- data.frame(SNP=as.character(onco_result_standard_5p$rs_id),CHR=onco_result_standard_5p$CHR,BP=onco_result_standard_5p$position,
                              P=onco_result_standard_5p$p_value1,stringsAsFactors =F)
   png(paste0("./man_standard.png"),width = 7.635,height =4.7175,units = "in",res = 600)
@@ -47,10 +50,10 @@ if(i1 == 1){
   # idx <- which(onco_result_standard_5p$exp_freq_a1>=0.2&
   #                onco_result_standard_5p$exp_freq_a1<=0.8)
   # onco_result_standard_20p <- onco_result_standard_5p[idx,]
-  
+  #polytomous model 
   p_value2 <- onco_result_standard_5p$p_value2
   p_value2[is.nan(p_value2)] <- 1
-  # too significant due the the insufficient 
+  # too significant due to the insufficient sample size
   idx <- which(p_value2 <= 1E-10)
   p_value2[idx] <- 1
   gwas_result4 <- data.frame(SNP=as.character(onco_result_standard_5p$rs_id),CHR=onco_result_standard_5p$CHR,BP=onco_result_standard_5p$position,
@@ -65,6 +68,18 @@ if(i1 == 1){
   dev.off()
   
   
+}else if(i1==5){
+  load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_casecase_grade_5p.Rdata")
+  #mixed_effect two-stage model with grade fixed
+  gwas_result2 <- data.frame(SNP=as.character(onco_result_casecase_5p$rs_id),CHR=onco_result_casecase_5p$CHR,BP=onco_result_casecase_5p$position,
+                             P=onco_result_casecase_5p$P,stringsAsFactors =F)
+  
+  png(paste0("./man_grade_fixed.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwas_result2,suggestiveline = F, cex.axis = 2,main=paste0("Global Test Association Manhattan Plot"),ymax=12)
+  dev.off()
+  png(paste0("./qq_grade_fixed.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  qq(gwas_result2$P,main=paste0("Global Test Association Manhattan QQ Plot"),ylim=c(0,12))
+  dev.off()
 }
 
 
