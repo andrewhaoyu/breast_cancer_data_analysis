7f9323a63da81235859a41020af3f7abbdc76a1b
Zhang <andrew.haoyu@gmail.com>
2019-09-27 10:07:15 -0400

    stat power high

--- simulation/power/code/stat_power_high.R
+++ simulation/power/code/stat_power_high.R
@@ -204,7 +204,11 @@ PowerCompare <- function(y.pheno.mis,G,x_covar,theta_intercept,theta_test,theta_
     model.standard <- glm(y.pheno.mis[,1]~G+x_covar,family = binomial(link='logit')) 
     p_standard <- summary(model.standard)$coefficients[2,4]
     
-    
+    idx.mis <- which(y.pheno.mis[,2]==888|y.pheno.mis[,3]==888|
+                       y.pheno.mis[,4]==888|y.pheno.mis[,5]==888)
+    y.pheno.com <- y.pheno.mis[-idx.mis,,drop=F]
+    x.covar.com <- x_covar[-idx.mis,drop=F]
+    G.com <- G[-idx.mis,drop=F]
     
     model2 <- TwoStageModel(y.pheno.com,additive=cbind(G.com,x.covar.com),missingTumorIndicator =NULL,delta0 = c(theta_intercept,theta_test,
                                                                                                                  theta_covar))
