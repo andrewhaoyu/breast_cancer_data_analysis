f1e630f5ed27846e1f2ab28faaf99cba6decd36d
Zhang <andrew.haoyu@gmail.com>
2019-10-03 09:28:38 -0400

    stat_power

--- simulation/power/code/test_effect.R
+++ simulation/power/code/test_effect.R
@@ -1,13 +1,13 @@
 if(s==1){
   #theta_test <- c(0.05,0,0,0,0)
-  theta_test <- c(0.07,0,0,0,0)
+  theta_test <- c(0.08,0,0,0,0)
   #theta_test <- c(0.25,0,0,0,0)
 }else if(s==2){
   #theta_test <- c(0,0.05,0,0,0)
-  theta_test <- c(0,0.07,0,0,0)
+  theta_test <- c(0,0.08,0,0,0)
 }else{
   #theta_test <- c(c(0,0.05),rnorm(3,0,0.02))
-  theta_test <- c(c(0,0.07),rnorm(3,0,0.02))
+  theta_test <- c(c(0,0.08),rnorm(3,0,0.02))
 }
 
     temp.simu <- SimulateDataPower(theta_intercept,theta_test,theta_covar,n)
