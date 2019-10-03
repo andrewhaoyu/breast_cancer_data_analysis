3f38ce97bac6c7d831e66d09834ca02dec363cfe
Zhang <andrew.haoyu@gmail.com>
2019-10-02 22:57:47 -0400

    stat_power

--- simulation/power/code/stat_power.r
+++ simulation/power/code/stat_power.r
@@ -312,15 +312,16 @@ result.list <- foreach(job.i = 1:2)%dopar%{
   for(s in 1:sc){
     if(s==1){
       #theta_test <- c(0.05,0,0,0,0)
-      theta_test <- c(0.07,0,0,0,0)
+      theta_test <- c(0.08,0,0,0,0)
       #theta_test <- c(0.25,0,0,0,0)
     }else if(s==2){
       #theta_test <- c(0,0.05,0,0,0)
-      theta_test <- c(0,0.07,0,0,0)
+      theta_test <- c(0,0.08,0,0,0)
     }else{
       #theta_test <- c(c(0,0.05),rnorm(3,0,0.02))
-      theta_test <- c(c(0,0.07),rnorm(3,0,0.02))
+      theta_test <- c(c(0,0.08),rnorm(3,0,0.02))
     }
+    
     for(n in sizes){
       for(i in 1:s_times){
         temp.simu <- SimulateDataPower(theta_intercept,theta_test,theta_covar,n)
