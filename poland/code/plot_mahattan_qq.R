e24f652ac7bae9a9bb3539f208a860bc3f817704
Zhang <andrew.haoyu@gmail.com>
2019-10-10 16:46:47 -0400

    plot manhattan two interaction

--- poland/code/plot_mahattan_qq.R
+++ poland/code/plot_mahattan_qq.R
@@ -80,7 +80,30 @@ if(i1 == 1){
   png(paste0("./qq_grade_fixed.png"),width = 7.635,height =4.7175,units = "in",res = 600)
   qq(gwas_result2$P,main=paste0("Global Test Association Manhattan QQ Plot"),ylim=c(0,12))
   dev.off()
+}else if(i1==6){
+  load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_fixed_5p_two_interaction.Rdata")
+  #fixed_effect two-stage model with all interactions
+  gwas_result1 <- data.frame(SNP=as.character(onco_result_fixed_5p$rs_id),CHR=onco_result_fixed_5p$CHR,BP=onco_result_fixed_5p$position,
+                             P=onco_result_fixed_5p$P,stringsAsFactors =F)
+  
+  png(paste0("./man_two_inter_fix.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwas_result1,suggestiveline = F, cex.axis = 2,main=paste0("Global Test Association Manhattan Plot"),ymax=12)
+  dev.off()
+  png(paste0("./qq_two_inter_fix.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  qq(gwas_result1$P,main=paste0("Global Test Association Manhattan QQ Plot"),ylim=c(0,12))
+}else if(i1==7){
+  load("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_random_5p_two_interaction.Rdata")
+  gwas_result2 <- data.frame(SNP=as.character(onco_result_casecase_5p$rs_id),CHR=onco_result_casecase_5p$CHR,BP=onco_result_casecase_5p$position,
+                             P=onco_result_casecase_5p$P,stringsAsFactors =F)
+  
+  png(paste0("./man_two_inter_mixed.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  manhattan(gwas_result2,suggestiveline = F, cex.axis = 2,main=paste0("Global Test Association Manhattan Plot"),ymax=12)
+  dev.off()
+  png(paste0("./qq_two_inter_mixed.png"),width = 7.635,height =4.7175,units = "in",res = 600)
+  qq(gwas_result2$P,main=paste0("Global Test Association Manhattan QQ Plot"),ylim=c(0,12))
+  dev.off()
 }
+
 
 
 
---
+++ poland/code/two_interaction_meta_merge.R
@@ -0,0 +1,20 @@
+#goal: merge meta-analysis p-values
+n <- nrow(onco_result_fixed_5p)
+p_value1 <- rep(0,n)
+p_value2 <- rep(0,n)
+total <- 0
+for(i1 in 1:1000){
+  print(i1)
+  load(paste0("/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/p_sub_two_interaction",i1,".Rdata"))
+  temp <- length(p_sub[[1]])
+  p_value1[total+(1:temp)] <- p_sub[[1]]
+  p_value2[total+(1:temp)] <- p_sub[[2]]
+  total = total+temp
+  
+}
+onco_result_fixed_5p <- cbind(onco_result_fixed_5p,p_value1)
+colnames(onco_result_fixed_5p)[144] <- "P"
+onco_result_casecase_5p <- cbind(onco_result_casecase_5p,p_value2)
+colnames(onco_result_casecase_5p)[102] <- "P"
+save(onco_result_fixed_5p,file="/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_fixed_5p_two_interaction.Rdata")
+save(onco_result_casecase_5p,file="/spin1/users/zhangh24/breast_cancer_data_analysis/poland/result/whole_genome/onco_result_random_5p_two_interaction.Rdata")
