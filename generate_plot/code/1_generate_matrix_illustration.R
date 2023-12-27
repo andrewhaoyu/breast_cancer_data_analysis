cbeea28aab874815d571929fabcc8161ab0c45b1
Haoyu Zhang <andrew.haoyu@gmail.com>
2023-12-27 13:15:15 -0500

    heter

--- generate_plot/code/1_generate_matrix_illustration.R
+++ generate_plot/code/1_generate_matrix_illustration.R
@@ -1,1 +1,109 @@
 #generate matrix illustration for Figure 1
+library(reshape2)
+library(ggplot2)
+library(RColorBrewer)
+source("./generate_plot/code/theme_publication.R")
+nrow = 8
+ncol = 10
+set.seed(6)
+A <- matrix(rbinom(nrow*ncol,2,0.33), 
+            byrow = TRUE, nrow = nrow, ncol = ncol)
+colnames(A) <- c(1:ncol)
+rownames(A) <- c(1:nrow)
+longData <- melt(A)
+fill_color = brewer.pal(9, "Greens")[c(2,5,8)]
+
+
+p1 = ggplot(longData, aes(x = Var2, y = Var1)) + 
+  geom_tile(aes(fill = as.character(value)), color = "black",size = 0.7) +  # Add black borders
+  scale_fill_manual(values = fill_color)+
+  labs(x = NULL, y = NULL, title = "Matrix") +  # Remove axis labels
+  theme_Publication() +
+  theme(
+    plot.margin = margin(0, 0, 0, 0, "cm"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    legend.position = "none",  # Remove legend
+    plot.title = element_blank(),
+    axis.line = element_blank()  # Remove axis lines
+  )
+png(file = "./generate_plot/result/genotype_matrix.png",
+    width = 3, height = 2, units = "in", res = 300)
+print(p1)
+dev.off()
+
+
+
+
+nrow = 8
+ncol = 1
+A <- matrix(c(c(1,1,1,1),c(0,0,0,0)), 
+            byrow = TRUE, nrow = nrow, ncol = ncol)
+colnames(A) <- c(1:ncol)
+rownames(A) <- c(1:nrow)
+longData <- melt(A)
+fill_color = brewer.pal(9, "Blues")[c(8,1)]
+
+
+p2 = ggplot(longData, aes(x = Var2, y = Var1)) + 
+  geom_tile(aes(fill = as.character(value)), color = "black",size = 0.7) +  # Add black borders
+  scale_fill_manual(values = fill_color)+
+  labs(x = NULL, y = NULL, title = "Matrix") +  # Remove axis labels
+  theme_Publication() +
+  theme(
+    plot.margin = margin(0, 0, 0, 0, "in"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    legend.position = "none",  # Remove legend
+    plot.title = element_blank(),
+    axis.line = element_blank()  # Remove axis lines
+  )
+png(file = "./generate_plot/result/phenotype_matrix.png",
+    width = 0.375, height = 2, units = "in", res = 300)
+print(p2)
+dev.off()
+
+
+
+
+
+
+nrow = 8
+ncol = 4
+A <- matrix(c(0,1,-9,1,888,888,888,888), 
+            byrow = FALSE, nrow = nrow, ncol = ncol)
+colnames(A) <- c(1:ncol)
+rownames(A) <- c(1:nrow)
+longData <- melt(A)
+fill_color = brewer.pal(9, "Blues")[c(8,1)]
+
+
+p2 = ggplot(longData, aes(x = Var2, y = Var1)) + 
+  geom_tile(aes(fill = as.character(value)), color = "black",size = 0.7) +  # Add black borders
+  scale_fill_manual(values = fill_color)+
+  labs(x = NULL, y = NULL, title = "Matrix") +  # Remove axis labels
+  theme_Publication() +
+  theme(
+    plot.margin = margin(0, 0, 0, 0, "in"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    legend.position = "none",  # Remove legend
+    plot.title = element_blank(),
+    axis.line = element_blank()  # Remove axis lines
+  )
+png(file = "./generate_plot/result/phenotype_matrix.png",
+    width = 0.375, height = 2, units = "in", res = 300)
+print(p2)
+dev.off()
---
+++ whole_genome_age/ICOG/Intrinsic_subtypes/code/1_intrinsic_subtypes_hetergeneity.R
@@ -0,0 +1,33 @@
+args = commandArgs(trailingOnly = T)
+i1 = as.numeric(args[[1]])
+#load the data
+load(paste0("/data/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/Intrinsic_subtypes/result/meta_result_shared_1p_082119.Rdata"))
+startend <- function(num,size,ind){
+  split.all <- split(1:num,cut(1:num,size))
+  temp <- split.all[[ind]]
+  start <- temp[1]
+  end <- temp[length(temp)]
+  return(c(start,end))
+}
+size = 1000
+num = nrow(meta_result_shared_1p)
+start_end_result = startend(num,size,i1)
+start = start_end_result[1]
+end = start_end_result[2]
+C = matrix(c(rep(1,5),
+             c(0,-1,0,0,0),
+             c(0,0,-1,0,0),
+             c(0,0,0,-1,0),
+             c(0,0,0,0,-1)),5,5)
+p_sub = rep(0,end-start+1)
+temp = 1
+for(k in start:end){
+  beta = as.numeric(meta_result_shared_1p[k,16:20])
+  beta_cov = matrix(as.numeric(meta_result_shared_1p[k,21:45]),5,5)
+  beta_heter = C%*%beta
+  cov_heter = C%*%beta_cov%*%t(C)
+  chi_test = t(beta_heter)%*%solve(cov_heter)%*%beta_heter
+  p_sub[temp] = pchisq(chi_test,length(beta_heter),lower.tail = T)
+  temp = temp + 1
+}
+save(p_sub, file = paste0("/data/zhangh24/breast_cancer_data_analysis/whole_genome_age/ICOG/Intrinsic_subtypes/result/p_heter_sub_",i1,".rdata"))
